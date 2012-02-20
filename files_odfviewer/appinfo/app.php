03bf9f109fb7c9eddb5b3faa50596067ce1dc2d5
Thomas Mueller <thomas.mueller@tmit.eu>
2012-02-20 23:23:40 +0100

    migrated from private repo contains first version of an ODF viewer for ownCloud

---
+++ files_odfviewer/appinfo/app.php
@@ -0,0 +1,6 @@
+<?php
+OC_Util::addStyle( 'files_odfviewer', 'webodf' );
+OC_Util::addStyle( 'files_odfviewer', 'odfviewer' );
+OC_Util::addScript('files_odfviewer', 'viewer' );
+OC_Util::addScript('files_odfviewer', 'webodf');
+?>
