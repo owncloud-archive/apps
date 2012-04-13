d12ba087ef1615a0208d3695a76dd8f62b316965
Thomas Müller <thomas@nautilus-lan.fritz.box>
2012-04-13 21:45:13 +0200

    initial

---
+++ mail/appinfo/app.php
@@ -0,0 +1,17 @@
+<?php
+
+OC_App::register( array(
+  'order' => 1,
+  'id' => 'mail',
+  'name' => 'Mail' ));
+
+OC_App::addNavigationEntry( array(
+  'id' => 'mail_index',
+  'order' => 1,
+  'href' => OC_Helper::linkTo( 'mail', 'index.php' ),
+  'icon' => OC_Helper::imagePath( 'mail', 'icon.png' ),
+  'name' => 'Mail'));
+
+OC_APP::registerPersonal('mail','settings');
+
+
---
+++ mail/index.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+* ownCloud - App Template Example
+*
+* @author Frank Karlitschek
+* @copyright 2011 Frank Karlitschek karlitschek@kde.org
+* 
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either 
+* version 3 of the License, or any later version.
+* 
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*  
+* You should have received a copy of the GNU Lesser General Public 
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+* 
+*/
+
+require_once('../../lib/base.php');
+
+// Check if we are a user
+if( !OC_User::isLoggedIn()){
+	header( "Location: ".OC_Helper::linkTo( '', 'index.php' ));
+	exit();
+}
+
+
+$somesetting=OC_Config::getValue( "somesetting", '' );
+OC_App::setActiveNavigationEntry( 'apptemplate');
+$tmpl = new OC_Template( 'apptemplate', 'main', 'user' );
+$tmpl->assign('somesetting',$somesetting);
+$tmpl->printPage();
+
+
+?>
