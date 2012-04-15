cff8ea8fca420eb8c664cb26353203eb94044ef2
Florian Jacob <fjacob@lavabit.com>
2012-04-15 19:45:10 +0200

    Fixed the generation of the login page redirect link.

--- user_openid_provider/provider.php
+++ user_openid_provider/provider.php
@@ -5,7 +5,9 @@ OC_Util::checkAppEnabled('user_openid_provider');
 
 $session = new OC_OpenIdProviderUserSession();
 $storage = new OC_OpenIdProviderStorage();
-$loginPage = OC_Helper::linkToAbsolute( '', 'index.php' ).'?redirect_url='.urlencode($_SERVER["REQUEST_URI"]);
+$loginPage = OC_Helper::linkToAbsolute( '', 'index.php' ).'?redirect_url='
+		.urlencode(OC_Helper::linkToAbsolute('user_openid_provider', 'provider.php').'?'
+				. $_SERVER["QUERY_STRING"]);
 $trustPage = OC_Helper::linkToAbsolute('user_openid_provider', 'trust.php');
 
 $server = new Zend_OpenId_Provider($loginPage, $trustPage, $session, $storage);
