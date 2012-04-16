c87e1f9b75fcf036e5203d42fe2b0a9cc61c8395
Florian Jacob <fjacob@lavabit.com>
2012-04-16 15:17:59 +0200

    Starting to handle the redirect to the login page on our own, as zend doesn't handle the redirect parameter correctly.

--- user_openid_provider/provider.php
+++ user_openid_provider/provider.php
@@ -5,12 +5,17 @@ OC_Util::checkAppEnabled('user_openid_provider');
 
 $session = new OC_OpenIdProviderUserSession();
 $storage = new OC_OpenIdProviderStorage();
-$loginPage = OC_Helper::linkToAbsolute( '', 'index.php' ).'?redirect_url='
-		.urlencode(OC_Helper::linkToAbsolute('user_openid_provider', 'provider.php').'?'
-				. $_SERVER["QUERY_STRING"]);
 $trustPage = OC_Helper::linkToAbsolute('user_openid_provider', 'trust.php');
 
 if ($_GET['openid.action']=='login') {
+	unset($_GET['openid.action']);
+	$params = '?';
+	foreach($_GET as $key => $value) {
+		$params .= '&' . $key . '=' . $value;
+	}
+	$loginPage = OC_Helper::linkToAbsolute( '', 'index.php' ).'?redirect_url='
+		.urlencode(OC_Helper::linkToAbsolute('user_openid_provider', 'provider.php')
+				. $params);
 	header('Location: '.$loginPage );
 }
 $server = new Zend_OpenId_Provider(null, $trustPage, $session, $storage);
