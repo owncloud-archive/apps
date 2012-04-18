4e9b82b7b0feb51339c38947e267bde4e97f1b59
Georg Ehrke <dev@georgswebsite.de>
2012-04-18 17:27:34 +0200

    move files to app folder

---
+++ files/l10n/sr@latin.php
@@ -0,0 +1,19 @@
+<?php $TRANSLATIONS = array(
+"There is no error, the file uploaded with success" => "Nema greške, fajl je uspešno poslat",
+"The uploaded file exceeds the upload_max_filesize directive in php.ini" => "Poslati fajl prevazilazi direktivu upload_max_filesize iz ",
+"The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form" => "Poslati fajl prevazilazi direktivu MAX_FILE_SIZE koja je navedena u HTML formi",
+"The uploaded file was only partially uploaded" => "Poslati fajl je samo delimično otpremljen!",
+"No file was uploaded" => "Nijedan fajl nije poslat",
+"Missing a temporary folder" => "Nedostaje privremena fascikla",
+"Files" => "Fajlovi",
+"Maximum upload size" => "Maksimalna veličina pošiljke",
+"Upload" => "Pošalji",
+"Nothing in here. Upload something!" => "Ovde nema ničeg. Pošaljite nešto!",
+"Name" => "Ime",
+"Download" => "Preuzmi",
+"Size" => "Veličina",
+"Modified" => "Zadnja izmena",
+"Delete" => "Obriši",
+"Upload too large" => "Pošiljka je prevelika",
+"The files you are trying to upload exceed the maximum size for file uploads on this server." => "Fajlovi koje želite da pošaljete prevazilaze ograničenje maksimalne veličine pošiljke na ovom serveru."
+);
