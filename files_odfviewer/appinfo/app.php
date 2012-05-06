439580d638b7dbcc122c6ee02ddc4d93be4e71f3
Thomas Müller <thomas.mueller@tmit.eu>
2012-05-06 12:53:52 +0200

    public api changes adopted

--- files_odfviewer/appinfo/app.php
+++ files_odfviewer/appinfo/app.php
@@ -1,6 +1,7 @@
 <?php
-OC_Util::addStyle( 'files_odfviewer', 'webodf' );
-OC_Util::addStyle( 'files_odfviewer', 'odfviewer' );
-OC_Util::addScript('files_odfviewer', 'viewer' );
-OC_Util::addScript('files_odfviewer', 'webodf');
+OCP\Util::addStyle( 'files_odfviewer', 'webodf' );
+OCP\Util::addStyle( 'files_odfviewer', 'odfviewer' );
+OCP\Util::addScript('files_odfviewer', 'viewer' );
+OCP\Util::addScript('files_odfviewer', 'webodf');
 ?>
+
