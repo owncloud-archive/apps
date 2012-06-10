2ffa3d64430763478b0c74786349c356034cde7a
Lukas Reschke <lukas@statuscode.ch>
2012-06-10 23:23:51 +0200

    Forget a )

--- apptemplate/settings.php
+++ apptemplate/settings.php
@@ -6,7 +6,7 @@ OC_Util::addScript( "apptemplate", "admin" );
 
 $tmpl = new OC_Template( 'apptemplate', 'settings');
 
-$tmpl->assign('url', htmlentities(OC_Config::getValue( "somesetting", '' ));
+$tmpl->assign('url', htmlentities(OC_Config::getValue( "somesetting", '' )));
 
 return $tmpl->fetchPage();
 
