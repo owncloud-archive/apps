6ef7447fa46d5aa19a33db9a5af938e590a04ac6
Robin Appelman <icewind@owncloud.com>
2012-08-29 03:11:00 +0200

    if we load webodf on demand, that means we don't need to always load it...

--- files_odfviewer/appinfo/app.php
+++ files_odfviewer/appinfo/app.php
@@ -2,4 +2,3 @@
 OCP\Util::addStyle( 'files_odfviewer', 'webodf' );
 OCP\Util::addStyle( 'files_odfviewer', 'odfviewer' );
 OCP\Util::addScript('files_odfviewer', 'viewer' );
-OCP\Util::addScript('files_odfviewer', 'webodf');
