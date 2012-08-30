13d1b1b7a81ad92b841482ff6eb7a2d44c08dc13
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-08-30 02:06:13 +0200

    [tx-robot] updated from transifex

---
+++ user_migrate/l10n/ko.php
@@ -0,0 +1,10 @@
+<?php $TRANSLATIONS = array(
+"Export" => "내보내기",
+"Something went wrong while the export file was being generated" => "내보내기 파일이 생성되는 동안 문제가 발생했습니다.",
+"An error has occurred" => "오류가 발생했습니다. ",
+"Export your user account" => "당신의 사용자 계정을 내보내세요.",
+"This will create a compressed file that contains your ownCloud account." => "이것으로 ownCloud 계정을 포함하는 압축 파일이 만들어집니다.",
+"Import user account" => "사용자 계정 가져오기",
+"ownCloud User Zip" => "ownCloud 사용자 Zip",
+"Import" => "가져오기"
+);
