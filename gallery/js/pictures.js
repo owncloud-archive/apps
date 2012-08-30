d0d66af8b0d8a793a60f5c4d02edd4c0c9d2d226
Robin Appelman <icewind@owncloud.com>
2012-08-30 22:41:13 +0200

    fix opening a gallery when using a custom port (oc-1597)

--- gallery/js/pictures.js
+++ gallery/js/pictures.js
@@ -61,8 +61,11 @@ function deplode(element) {
 
 function openNewGal(album_name) {
 	root = root + decodeURIComponent(album_name) + "/";
-	var url = window.location.protocol+"//"+window.location.hostname+OC.linkTo('gallery', 'index.php');
-	url = url + "?root="+encodeURIComponent(root);
+	var url = OC.linkTo('gallery', 'index.php');
+	url = url + "&root="+encodeURIComponent(root);
+	//extract only the query part
+	var query=url.substr(url.indexOf('?'));
+	console.log(query);
 
-	window.location = url;
+	window.location.search = query;
 }
