29877e68293539dc750df09897a39468224bbdd1
Thomas Mueller <thomas.mueller@tmit.eu>
2012-09-02 19:34:59 +0200

    Adding missing translation

--- admin_migrate/templates/settings.php
+++ admin_migrate/templates/settings.php
@@ -1,14 +1,15 @@
 <form id="export" action="#" method="post">
     <fieldset class="personalblock">
         <legend><strong><?php echo $l->t('Export this ownCloud instance');?></strong></legend>
-        <p><?php echo $l->t('This will create a compressed file that contains the data of this owncloud instance.
-            Please choose the export type:');?>
-        </p>
-        <h3>What would you like to export?</h3>
         <p>
-        <input type="radio" name="export_type" value="instance" style="width:20px;" /> ownCloud instance (suitable for import )<br />
-	<input type="radio" name="export_type" value="system" style="width:20px;" /> ownCloud system files<br />
-	<input type="radio" name="export_type" value="userfiles" style="width:20px;" /> Just user files<br />
+            <?php echo $l->t('This will create a compressed file that contains the data of this owncloud instance.');?>
+            <br />
+            <?php echo $l->t('Please choose the export type:');?>
+        </p>
+        <p>
+        <input type="radio" name="export_type" value="instance" style="width:20px;" /><?php echo $l->t('ownCloud instance (suitable for import)');?><br />
+	<input type="radio" name="export_type" value="system" style="width:20px;" /><?php echo $l->t('ownCloud system files');?><br />
+	<input type="radio" name="export_type" value="userfiles" style="width:20px;" /><?php echo $l->t('Just user files');?><br />
         <input type="submit" name="admin_export" value="<?php echo $l->t('Export'); ?>" />
     </fieldset>
 </form>
