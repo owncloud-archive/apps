9ed60cf450ae438e296213200ed8c326ca7bbc80
Thomas Tanghus <thomas@tanghus.net>
2012-09-02 15:58:20 +0200

    Shared calendars are already in OC_Calendar_Calendar::allCalendars()

--- calendar/index.php
+++ calendar/index.php
@@ -22,18 +22,6 @@ foreach($calendars as $calendar){
 	}
 }
 
-$events_baseURL = OCP\Util::linkTo('calendar', 'ajax/events.php');
-$eventSources[] = array('url' => $events_baseURL.'?calendar_id=shared_rw',
-		'backgroundColor' => '#1D2D44',
-		'borderColor' => '#888',
-		'textColor' => 'white',
-		'editable'=>'true');
-$eventSources[] = array('url' => $events_baseURL.'?calendar_id=shared_r',
-		'backgroundColor' => '#1D2D44',
-		'borderColor' => '#888',
-		'textColor' => 'white',
-		'editable' => 'false');
-
 OCP\Util::emitHook('OC_Calendar', 'getSources', array('sources' => &$eventSources));
 $categories = OC_Calendar_App::getCategoryOptions();
 
