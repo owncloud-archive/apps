c23b3a0cb099e7909f5dc8bd61476fad4711cbea
Michal Jaskurzynski <mjaskurzynski@gmail.com>
2012-09-02 18:38:28 +0200

    first official version of Mozilla Sync app

---
+++ mozilla_sync/templates/noemail.php
@@ -0,0 +1,4 @@
+<fieldset class="personalblock">
+	<legend><?php echo $l->t('Mozilla Sync Service'); ?></legend>
+  <?php echo $l->t('Please set your email address in account settings');?>
+</fieldset>
