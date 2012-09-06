f0d389b4a54eab3de8c11ba8e4a88492d1cda1ae
Georg Ehrke <dev@georgswebsite.de>
2012-09-06 11:59:40 +0200

    some Calendar UI improvements

--- calendar/templates/calendar.php
+++ calendar/templates/calendar.php
@@ -43,8 +43,8 @@
 					</form>
 					<form id="choosecalendar">
 						<!--<input type="button" id="today_input" value="<?php echo $l->t("Today");?>"/>-->
-						<a class="settings calendarsettings" title="<?php echo $l->t('Calendars'); ?>"><img class="svg" src="<?php echo OCP\Util::imagePath('calendar', 'icon.svg'); ?>" alt="<?php echo $l->t('Calendars'); ?>" /></a>
-						<a class="settings generalsettings" title="<?php echo $l->t('Settings'); ?>"><img class="svg" src="<?php echo OCP\Util::imagePath('core', 'actions/settings.svg'); ?>" alt="<?php echo $l->t('Settings'); ?>" /></a>
+						<button type="button"><a class="settings calendarsettings" title="<?php echo $l->t('Calendars'); ?>"><img class="svg" src="<?php echo OCP\Util::imagePath('calendar', 'icon.svg'); ?>" alt="<?php echo $l->t('Calendars'); ?>" /></a></button>
+						<button type="button"><a class="settings generalsettings" title="<?php echo $l->t('Settings'); ?>"><img class="svg" src="<?php echo OCP\Util::imagePath('core', 'actions/settings.svg'); ?>" alt="<?php echo $l->t('Settings'); ?>" /></a></button>
 					</form>
 					<form id="datecontrol">
 						<input type="button" value="&nbsp;&lt;&nbsp;" id="datecontrol_left"/>
