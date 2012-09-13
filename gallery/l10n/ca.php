4bf5eff67cddc4af31ee307ec90c8fa43dc88e42
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-13 02:04:07 +0200

    [tx-robot] updated from transifex

--- gallery/l10n/ca.php
+++ gallery/l10n/ca.php
@@ -1,7 +1,9 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Fotos",
+"Galleries" => "Galeries",
 "Share gallery" => "Comperteix la galeria",
 "Error: " => "Error: ",
 "Internal error" => "Error intern",
-"Slideshow" => "Passi de diapositives"
+"Slideshow" => "Passi de diapositives",
+"The \"Image Viewer\" application also need to be enabled to use this application." => "L'apliació \"Visualitzador d'Imatges\" també ha d'estar activada per a poder utilitzar aquesta aplicació."
 );
