e4b0b12aa273360c169fec9f01eda07cc786f6a0
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-14 02:04:08 +0200

    [tx-robot] updated from transifex

---
+++ files_pdfviewer/l10n/da.php
@@ -0,0 +1,8 @@
+<?php $TRANSLATIONS = array(
+"Previous" => "Forrige",
+"Next" => "Næste",
+"Page Width" => "Sidebredde",
+"Page Fit" => "Tilpas side",
+"Print" => "Udskriv",
+"Download" => "Download"
+);
