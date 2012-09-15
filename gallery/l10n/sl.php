70b4d8dff947e33c905bc93ab15b24433a0e0395
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-15 02:07:46 +0200

    [tx-robot] updated from transifex

--- gallery/l10n/sl.php
+++ gallery/l10n/sl.php
@@ -4,6 +4,6 @@
 "Share gallery" => "Daj galerijo v souporabo",
 "Error: " => "Napaka: ",
 "Internal error" => "Notranja napaka",
-"Slideshow" => "predstavitev",
+"Slideshow" => "Predstavitev",
 "The \"Image Viewer\" application also need to be enabled to use this application." => "Za uporabo te aplikacije mora biti omogočena tudi aplikacija \"Pregledovalnik slik\"."
 );
