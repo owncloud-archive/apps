8b5269d2e58b8982a181d43ce344d86d47b60917
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-09-17 17:01:13 +0200

    [tx-robot] updated from transifex

---
+++ user_migrate/l10n/zh_CN.GB2312.php
@@ -0,0 +1,10 @@
+<?php $TRANSLATIONS = array(
+"Export" => "导出",
+"Something went wrong while the export file was being generated" => "生成导出文件时出错",
+"An error has occurred" => "发生了一个错误",
+"Export your user account" => "导出您的用户帐号",
+"This will create a compressed file that contains your ownCloud account." => "这将创建一个包含您的 ownCloud 帐号的压缩文件：",
+"Import user account" => "导入用户帐号",
+"ownCloud User Zip" => "ownCloud 用户 Zip",
+"Import" => "导入"
+);
