2d458ccbd3be6c2ce783e485a3a1be83f60ffb71
Bart Visscher <bartv@thisnet.nl>
2012-10-03 21:44:07 +0200

    Mail: correct checking for errors in ajax/messages

--- mail/ajax/messages.php
+++ mail/ajax/messages.php
@@ -28,7 +28,7 @@ $account_id = isset( $_GET['account_id'] ) ? $_GET['account_id'] : null;
 $folder_id = isset( $_GET['folder_id'] ) ? $_GET['folder_id'] : null;
 
 $messages = OCA_Mail\App::getMessages( OCP\User::getUser(), $account_id, $folder_id );
-if( $messages['error'] ) {
+if( isset($messages['error']) ) {
 	OCP\JSON::error(array('data' => array('message' => $messages['error'] )));
 	exit();
 }
