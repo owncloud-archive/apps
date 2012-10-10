a11c125636f4be7321912c92b22b34e254f1c92e
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-10 02:12:53 +0200

    [tx-robot] updated from transifex

---
+++ user_openid/l10n/he.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"Identity: <b>" => "זהות: <b>",
+"User: <b>" => "שם משתמש: <b>",
+"Login" => "התחברות",
+"Authorized OpenID provider" => "ספק OpenID מורשה",
+"Your address at Wordpress, Identi.ca, &hellip;" => "הכתובת שלך ב־WordPress,‏ Identi.ca,‏ &hellip;"
+);
