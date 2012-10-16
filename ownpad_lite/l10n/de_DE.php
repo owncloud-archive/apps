dd4440cf08ada8ecf9dab33faf000fdf4d0b4309
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-16 23:34:50 +0200

    [tx-robot] updated from transifex

---
+++ ownpad_lite/l10n/de_DE.php
@@ -0,0 +1,9 @@
+<?php $TRANSLATIONS = array(
+"My pad" => "Mein Pad",
+"Settings" => "Einstellungen",
+"Pad Title" => "Titel des Pads",
+"Open" => "Öffnen",
+"Etherpad service URL" => "URL des Etherpad-Service",
+"Username" => "Benutzername",
+"Save" => "Speichern"
+);
--- user_migrate/l10n/ru_RU.php
+++ user_migrate/l10n/ru_RU.php
@@ -5,5 +5,6 @@
 "Export your user account" => "Экспортировать Ваш пользовательский аккаунт",
 "This will create a compressed file that contains your ownCloud account." => "Это создаст сжатый файл, содержащий Ваш ownCloud аккаунт.",
 "Import user account" => "Импортировать пользовательскую учетную запись",
+"ownCloud User Zip" => "ownCloud пользовательский Zip",
 "Import" => "Импортировать"
 );
