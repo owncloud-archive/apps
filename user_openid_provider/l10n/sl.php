681ad3e1f0491bbcef65118e76413e770a712ef1
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-10-23 02:11:00 +0200

    [tx-robot] updated from transifex

---
+++ user_openid_provider/l10n/sl.php
@@ -0,0 +1,6 @@
+<?php $TRANSLATIONS = array(
+"OpenID Provider remembered sites" => "Shranjene strani ponudnika OpenID",
+"Trusted" => "Zaupano",
+"Denied" => "Zavrnjeno",
+"Remove" => "Odstrani"
+);
