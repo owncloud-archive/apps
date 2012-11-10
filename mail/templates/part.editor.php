e985c59adbcb6416f273a489d3b7e640bc68bea3
Thomas Mueller <thomas.mueller@tmit.eu>
2012-11-10 23:07:32 +0100

    started to add autocomplete to 'To:' field

--- mail/templates/part.editor.php
+++ mail/templates/part.editor.php
@@ -28,8 +28,8 @@
     $(function () {
         $("#mail_editor").dialog({
             autoOpen:false,
-            height:300,
-            width:350,
+            height:420,
+            width:640,
             modal:true,
             buttons:{
                 "Send":function () {
@@ -37,4 +37,40 @@
                 }
             }});
     });
+
+    $( "#to" ).autocomplete({
+        source: function( request, response ) {
+            $.ajax({
+                url: "http://ws.geonames.org/searchJSON",
+                dataType: "jsonp",
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    maxRows: 12,
+                    name_startsWith: request.term
+                },
+                success: function( data ) {
+                    response( $.map( data.geonames, function( item ) {
+                        return {
+                            label: item.name + (item.adminName1 ? ", " + item.adminName1 : "") + ", " + item.countryName,
+                            value: item.name
+                        }
+                    }));
+                }
+            });
+        },
+        minLength: 2,
+        select: function( event, ui ) {
+//            log( ui.item ?
+//                    "Selected: " + ui.item.label :
+//                    "Nothing selected, input was " + this.value);
+        },
+        open: function() {
+            $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+            $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
+    });
+
 </script>
