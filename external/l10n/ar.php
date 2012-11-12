ae17e3f922acca07819b1ca746d7acaa473e49fb
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-13 00:05:35 +0100

    [tx-robot] updated from transifex

---
+++ external/l10n/ar.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"External Sites" => "مواقع خارجية",
+"Name" => "اسم",
+"URL" => "عنوان الموقع",
+"Remove site" => "ازل الموقع",
+"Add" => "اضف"
+);
---
+++ files_texteditor/l10n/ar.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"Save" => "احفظ",
+"Close" => "اغلق",
+"Saving..." => "حفظ",
+"An error occurred!" => "حدث خطأ!",
+"There were unsaved changes, click here to go back" => "هناك تغييرات غير محفوظة، اضغط هنا للعودة"
+);
