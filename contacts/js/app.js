e4a2bf348ee636f28b2a13bebffd65e30adc6f1e
Thomas Tanghus <thomas@tanghus.net>
2012-11-22 00:24:31 +0100

    Array.prototype.clean() work on clone of self.

--- contacts/js/app.js
+++ contacts/js/app.js
@@ -60,14 +60,19 @@ if (typeof Object.create !== 'function') {
 	};
 }
 
+Array.prototype.clone = function() {
+  return this.slice(0);
+};
+
 Array.prototype.clean = function(deleteValue) {
-	for (var i = 0; i < this.length; i++) {
-		if (this[i] == deleteValue) {
-			this.splice(i, 1);
+	var arr = this.clone();
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] == deleteValue) {
+			arr.splice(i, 1);
 			i--;
 		}
 	}
-	return this;
+	return arr;
 };
 
 // Keep it DRY ;)
