698f67d379499149f0bf67a00f26ce94d6845e7b
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2012-11-30 00:03:18 +0100

    [tx-robot] updated from transifex

--- ownpad_lite/l10n/gl.php
+++ ownpad_lite/l10n/gl.php
@@ -1,9 +1,9 @@
 <?php $TRANSLATIONS = array(
-"My pad" => "O meu pad",
+"My pad" => "O meu bloque",
 "Settings" => "Configuración",
-"Pad Title" => "Título do pad",
+"Pad Title" => "Título do bloque",
 "Open" => "Abrir",
-"Etherpad service URL" => "URL do servizo etherpad",
+"Etherpad service URL" => "URL do servizo Etherpad",
 "Username" => "Nome de usuario",
 "Save" => "Gardar"
 );
