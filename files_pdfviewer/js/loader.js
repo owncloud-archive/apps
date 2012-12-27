fd59676b315791888f4107ac411be599761abe02
Thomas Mueller <thomas.mueller@tmit.eu>
2012-12-27 11:00:25 +0100

    [pdf_viewer] fixes #252

--- files_pdfviewer/js/loader.js
+++ files_pdfviewer/js/loader.js
@@ -9,11 +9,10 @@ function hidePDFviewer() {
 function showPDFviewer(dir,filename){
 	if(!showPDFviewer.shown){
 		$("#editor").hide();
-		var url = fileDownloadPath(dir, filename);
 		$('#content table').hide();
 		$("#controls").hide();
 		var oldcontent = $("#content").html();
-        var viewer = OC.linkTo('files_pdfviewer','viewer.php')+'&dir='+dir+'&file='+filename;
+        var viewer = OC.linkTo('files_pdfviewer', 'viewer.php')+'&dir='+encodeURIComponent(dir).replace(/%2F/g, '/')+'&file='+encodeURIComponent(filename);
 		$("#content").html(oldcontent+'<iframe style="width:100%;height:100%;display:block;" src="'+viewer+'" />');
 		$("#pageWidthOption").attr("selected","selected");
 	}
