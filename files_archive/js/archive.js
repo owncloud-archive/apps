e7729c717d9eff3abb7a0809278adbb045a32ab6
Thomas Mueller <thomas.mueller@tmit.eu>
2013-01-15 00:49:03 +0100

    fixes https://github.com/owncloud/core/issues/1175 in stable45

--- files_archive/js/archive.js
+++ files_archive/js/archive.js
@@ -6,22 +6,24 @@
  */
 
 $(document).ready(function() {
-	if(typeof FileActions!=='undefined'){
-		FileActions.register('application/zip','Open', OC.PERMISSION_READ, '',function(filename){
-			window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
-		});
-		FileActions.setDefault('application/zip','Open');
-		FileActions.register('application/x-gzip','Open', OC.PERMISSION_READ, '',function(filename){
-			window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
-		});
-		FileActions.setDefault('application/x-compressed','Open');
-		FileActions.register('application/x-compressed','Open', OC.PERMISSION_READ, '',function(filename){
-			window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
-		});
-		FileActions.setDefault('application/x-compressed','Open');
-		FileActions.register('application/x-tar','Open', OC.PERMISSION_READ, '',function(filename){
-			window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
-		});
-		FileActions.setDefault('application/x-tar','Open');
+	if(OC.currentUser) {
+		if(typeof FileActions!=='undefined'){
+			FileActions.register('application/zip','Open', OC.PERMISSION_READ, '',function(filename){
+				window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+			});
+			FileActions.setDefault('application/zip','Open');
+			FileActions.register('application/x-gzip','Open', OC.PERMISSION_READ, '',function(filename){
+				window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+			});
+			FileActions.setDefault('application/x-compressed','Open');
+			FileActions.register('application/x-compressed','Open', OC.PERMISSION_READ, '',function(filename){
+				window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+			});
+			FileActions.setDefault('application/x-compressed','Open');
+			FileActions.register('application/x-tar','Open', OC.PERMISSION_READ, '',function(filename){
+				window.location=OC.linkTo('files', 'index.php')+'&dir='+encodeURIComponent($('#dir').val()).replace(/%2F/g, '/')+'/'+encodeURIComponent(filename);
+			});
+			FileActions.setDefault('application/x-tar','Open');
+		}
 	}
 });
