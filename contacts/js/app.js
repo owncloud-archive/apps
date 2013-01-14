d2797f56eac39b4d3e5287c54b5db13a5a221405
Thomas Tanghus <thomas@tanghus.net>
2013-01-14 09:15:49 +0100

    Contacts: Disable touchmove on body element.

--- contacts/js/app.js
+++ contacts/js/app.js
@@ -1120,6 +1120,10 @@ OC.Contacts = OC.Contacts || {
 			});
 		});
 
+		$('body').on('touchmove', function(event) {
+			event.preventDefault();
+		});
+		
 		$(document).on('keypress', function(event) {
 			if(!$(event.target).is('body')) {
 				return;
