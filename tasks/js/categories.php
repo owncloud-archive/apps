25268628904de7487f53b54002370fa231e5b86c
Lukas Reschke <lukas@statuscode.ch>
2013-01-21 23:30:10 +0100

    Tasks - Move inline JS to external file

---
+++ tasks/js/categories.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright (c) 2013 Lukas Reschke <lukas@statuscode.ch>
+ * This file is licensed under the Affero General Public License version 3 or
+ * later.
+ * See the COPYING-README file.
+ */
+
+// Set the content type to Javascript
+header("Content-type: text/javascript");
+
+// Disallow caching
+header("Cache-Control: no-cache, must-revalidate"); 
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+
+$categories = OC_Calendar_App::getCategoryOptions();
+echo ("var categories =". json_encode($categories));
