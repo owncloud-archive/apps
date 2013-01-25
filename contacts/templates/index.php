d2c89c2fcc4ebd2123926aed3d9ebf8f695cd62c
Lukas Reschke <lukas@statuscode.ch>
2013-01-25 16:33:41 +0100

    Undo uneeded changes

--- contacts/templates/index.php
+++ contacts/templates/index.php
@@ -1,4 +1,10 @@
 <div id='notification'></div>
+<script type='text/javascript'>
+	var totalurl = '<?php echo OCP\Util::linkToRemote('carddav'); ?>addressbooks';
+	var categories = <?php echo json_encode($_['categories']); ?>;
+	var id = '<?php echo $_['id']; ?>';
+	var lang = '<?php echo OCP\Config::getUserValue(OCP\USER::getUser(), 'core', 'lang', 'en'); ?>';
+</script>
 <div id="leftcontent">
 	<div class="hidden" id="statusbar"></div>
 	<div id="contacts">
