c452d6ef3ea746b442f5e8f80f3b415c75bfa9d4
Thomas Tanghus <thomas@tanghus.net>
2013-01-29 20:14:39 +0100

    Contacts: Use OCP\Contacts for search.

--- contacts/lib/search.php
+++ contacts/lib/search.php
@@ -4,20 +4,12 @@ namespace OCA\Contacts;
 
 class SearchProvider extends \OC_Search_Provider{
 	function search($query) {
-		$addressbooks = Addressbook::all(\OCP\USER::getUser(), 1);
-		if(count($addressbooks)==0 || !\OCP\App::isEnabled('contacts')) {
-			return array();
-		}
-		$results=array();
+		$results = \OCP\Contacts::search($query, array('N', 'FN', 'EMAIL', 'NICKNAME', 'ORG'));
 		$l = new \OC_l10n('contacts');
-		foreach($addressbooks as $addressbook) {
-			$vcards = VCard::all($addressbook['id']);
-			foreach($vcards as $vcard) {
-				if(substr_count(strtolower($vcard['fullname']), strtolower($query)) > 0) {
-					$link = \OCP\Util::linkTo('contacts', 'index.php').'#'.urlencode($vcard['id']);
-					$results[]=new \OC_Search_Result($vcard['fullname'], '', $link, (string)$l->t('Contact'));//$name,$text,$link,$type
-				}
-			}
+		foreach($results as $result) {
+			$vcard = VCard::find($result['id']);
+			$link = \OCP\Util::linkTo('contacts', 'index.php').'#' . $vcard['id'];
+			$results[]=new \OC_Search_Result($vcard['fullname'], '', $link, (string)$l->t('Contact'));//$name,$text,$link,$type
 		}
 		return $results;
 	}
