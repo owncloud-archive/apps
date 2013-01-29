6311db49a3bad499047f0c9a0bed74414fe74000
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-01-30 00:22:14 +0100

    [tx-robot] updated from transifex

--- files_antivirus/l10n/eu.php
+++ files_antivirus/l10n/eu.php
@@ -1,10 +1,14 @@
 <?php $TRANSLATIONS = array(
+"Greetings {user}," => "Kaixo {user},",
+"File uploaded: {file}" => "Igotako fitxategia: {file}",
 "Antivirus Configuration" => "Antivirus konfigurazioa",
 "Mode" => "Modua",
 "Executable" => "Abiarazgarria",
 "Daemon" => "Deabrua",
 "Host" => "Hostalaria",
+"Address of Antivirus Host." => "Antiburus Hostalariaren Helbidea.",
 "Port" => "Portua",
+"Port number of Antivirus Host." => "Antiburus Hostalariaren Ataka zenbakia.",
 "Path to clamscan" => "clamscan bidea",
 "Delete file" => "Ezabatu fitxaegia",
 "Save" => "Gorde"
