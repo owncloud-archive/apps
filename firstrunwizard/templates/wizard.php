4c81b654a1aa7527fa48d834417810547ff8b581
Lukas Reschke <lukas@statuscode.ch>
2013-01-30 15:15:54 +0100

    Some translations

--- firstrunwizard/templates/wizard.php
+++ firstrunwizard/templates/wizard.php
@@ -4,11 +4,11 @@
 	<img class="svg" src="/owncloud/core/img/actions/delete.svg">
 </a>
 
-<h1>Welcome to ownCloud</h1>
-<p>Your personal web services. All your files, contacts, calendar and more, in one place.</p>
+<h1><?php echo $l->t('Welcome to ownCloud');?></h1>
+<p><?php echo $l->t('Your personal web services. All your files, contacts, calendar and more, in one place.');?></p>
 
 
-<h2>Get the apps to sync your files</h2>
+<h2><?php echo $l->t('Get the apps to sync your files');?></h2>
 <a href="http://owncloud.org/sync-clients/">
 	<img src="<?php echo OCP\Util::imagePath('firstrunwizard', 'desktopapp.png'); ?>" />
 </a>
@@ -20,15 +20,15 @@
 </a>
 
 
-<h2>Connect your desktop apps to ownCloud</h2>
+<h2><?php echo $l->t('Connect your desktop apps to ownCloud');?></h2>
 <a class="button" href="http://doc.owncloud.org/server/5.0/user_manual/calendars.html#synchronising-calendars-with-caldav">
-	<img class="appsmall" src="<?php echo OCP\Util::imagePath('calendar', 'icon.svg'); ?>" /> Connect your Calendar
+	<img class="appsmall" src="<?php echo OCP\Util::imagePath('calendar', 'icon.svg'); ?>" /> <?php echo $l->t('Connect your Calendar');?>
 </a>
 <a class="button" href="http://doc.owncloud.org/server/5.0/user_manual/contacts.html#keeping-your-address-book-in-sync">
-	<img class="appsmall" src="<?php echo OCP\Util::imagePath('settings', 'users.svg'); ?>" /> Connect your Contacts
+	<img class="appsmall" src="<?php echo OCP\Util::imagePath('settings', 'users.svg'); ?>" /> <?php echo $l->t('Connect your Contacts');?>
 </a>
 <a class="button" href="http://doc.owncloud.org/server/5.0/user_manual/connecting_webdav.html">
-	<img class="appsmall" src="<?php echo OCP\Util::imagePath('core', 'places/folder.svg'); ?>" /> Access files via WebDAV
+	<img class="appsmall" src="<?php echo OCP\Util::imagePath('core', 'places/folder.svg'); ?>" /> <?php echo $l->t('Access files via WebDAV');?>
 </a>
 
 <p class="footnote">There’s more information in the <a href="http://doc.owncloud.org/server/5.0/user_manual/">documentation</a> and on our <a href="http://owncloud.org">website</a>.<br>
