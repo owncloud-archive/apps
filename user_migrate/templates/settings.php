2de405a08faa79e11b2ac9c0916061ee9c8ee188
Lukas Reschke <lukas@statuscode.ch>
2013-02-03 19:46:59 +0100

    [user_migrate] Add requesttoken

--- user_migrate/templates/settings.php
+++ user_migrate/templates/settings.php
@@ -12,6 +12,7 @@
 		<?php } ?>
         <legend><strong><?php echo $l->t('Import user account');?></strong></legend>
         </p>
+        <input type="hidden" name="requesttoken" value="<?php echo $_['requesttoken'] ?>" id="requesttoken">
         <p><input type="file" id="owncloud_import" name="owncloud_import" style="width:280px;"><label for="owncloud_import"> <?php echo $l->t('ownCloud User Zip');?></label>
         </p>
         <input type="submit" name="user_import" value="<?php echo $l->t('Import'); ?>" />
