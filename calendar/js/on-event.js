952b20d7dae849c1300decc039c87f3f95cfc513
Georg Ehrke <dev@georgswebsite.de>
2013-02-06 12:06:40 +0100

    fix actions with edited events

--- calendar/js/on-event.js
+++ calendar/js/on-event.js
@@ -31,13 +31,14 @@ $('.activeCalendar').live('click', function () {
 $('#allday_checkbox').live('click', function () {
 	Calendar.UI.lockTime();
 });
-$('.editEvent-submit').live('click', function () {
+$('#editEvent-submit').live('click', function () {
+	console.log('submit-event');
 	Calendar.UI.validateEventForm($(this).data('link'));
 });
-$('.editEvent-delete').live('click', function () {
+$('#editEvent-delete').live('click', function () {
 	Calendar.UI.submitDeleteEventForm($(this).data('link'));
 });
-$('.editEvent-export').live('click', function () {
+$('#editEvent-export').live('click', function () {
 	window.location = $(this).data('link');
 });
 $('#chooseCalendar-showCalDAVURL').live('click', function () {
--- calendar/templates/part.editevent.php
+++ calendar/templates/part.editevent.php
@@ -5,9 +5,9 @@
 <?php echo $this->inc("part.eventform"); ?>
 	<div style="width: 100%;text-align: center;color: #FF1D1D;" id="errorbox"></div>
 	<span id="actions">
-		<input type="button" class="submit" style="float: left;" class="editEvent-submit" value="<?php echo $l->t("Submit");?>" data-link="<?php echo OCP\Util::linkTo('calendar', 'ajax/event/edit.php') ?>">
-		<input type="button" class="submit" style="float: left;" class="editEvent-delete"  name="delete" value="<?php echo $l->t("Delete");?>" data-link="<?php echo OCP\Util::linkTo('calendar', 'ajax/event/delete.php') ?>">
-		<input type="button" class="submit" style="float: right;" class="editEvent-export"  name="export" value="<?php echo $l->t("Export");?>" data-link="<?php echo OCP\Util::linkTo('calendar', 'export.php') ?>?eventid=<?php echo $_['eventid'] ?>">
+		<input type="button" class="submit" style="float: left;" id="editEvent-submit" value="<?php echo $l->t("Submit");?>" data-link="<?php echo OCP\Util::linkTo('calendar', 'ajax/event/edit.php') ?>">
+		<input type="button" class="submit" style="float: left;" id="editEvent-delete"  name="delete" value="<?php echo $l->t("Delete");?>" data-link="<?php echo OCP\Util::linkTo('calendar', 'ajax/event/delete.php') ?>">
+		<input type="button" class="submit" style="float: right;" id="editEvent-export"  name="export" value="<?php echo $l->t("Export");?>" data-link="<?php echo OCP\Util::linkTo('calendar', 'export.php') ?>?eventid=<?php echo $_['eventid'] ?>">
 	</span>
 	</form>
 </div>
