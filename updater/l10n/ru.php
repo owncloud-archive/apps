28204533d8e4d0fa9deb2a742a6d7253f06ede73
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-02-08 00:09:21 +0100

    [tx-robot] updated from transifex

--- updater/l10n/ru.php
+++ updater/l10n/ru.php
@@ -1,4 +1,6 @@
 <?php $TRANSLATIONS = array(
+"In progress..." => "В процессе...",
+"Done. Reload the page to proceed." => "Сделано. Обновите страницу для продолжения.",
 "Error" => "Ошибка",
 "Update" => "Обновить"
 );
--- user_migrate/l10n/ru.php
+++ user_migrate/l10n/ru.php
@@ -2,5 +2,6 @@
 "Export" => "Экспорт",
 "Something went wrong while the export file was being generated" => "В процессе создания файла экспорта что-то пошло не так",
 "An error has occurred" => "Произошла ошибка",
+"User Account Migration" => "Перемещение аккаунта пользователя",
 "Export and import ownCloud user accounts." => "Экспорт и импорт учетных записей пользователей"
 );
