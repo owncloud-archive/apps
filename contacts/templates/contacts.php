b27fb8066b3a99c549cb10e3c966d6882bd85f5d
Thomas Tanghus <thomas@tanghus.net>
2013-02-08 15:24:53 +0100

    Contacts: Settings fix.

--- contacts/templates/contacts.php
+++ contacts/templates/contacts.php
@@ -14,11 +14,11 @@
 	</nav>
 	<div id="contacts-settings">
 			<h3 class="settings action text" tabindex="0" role="button" title="<?php echo $l->t('Settings'); ?>"></h3>
-			<h3 data-id="addressbooks" tabindex="0" role="button"><?php echo $l->t('Address books'); ?></h3>
+			<h2 data-id="addressbooks" tabindex="0" role="button"><?php echo $l->t('Address books'); ?></h3>
 				<ul class="hidden">
 				</ul>
-			<h3 data-id="import" tabindex="0" role="button"><?php echo $l->t('Import'); ?></h3>
-				<ul class="hidden">
+			<h2 data-id="import" tabindex="0" role="button"><?php echo $l->t('Import'); ?></h3>
+				<ul>
 					<li class="import-upload">
 						<form id="import_upload_form" action="<?php echo OCP\Util::linkTo('contacts', 'ajax/uploadimport.php'); ?>" method="post" enctype="multipart/form-data" target="import_upload_target">
 						<input type="hidden" name="MAX_FILE_SIZE" value="<?php echo $_['uploadMaxFilesize'] ?>" id="max_upload">
