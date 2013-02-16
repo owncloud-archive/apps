f3472b8b57fe1340735d638454755684a752adb2
Bernhard Posselt <nukeawhale@gmail.com>
2013-02-16 23:29:47 +0100

    updated app js

--- apptemplateadvanced/js/app.js
+++ apptemplateadvanced/js/app.js
@@ -49,18 +49,6 @@
     }
   ]);
 
-  angular.module('AppTemplateAdvanced').run([
-    '$rootScope', function($rootScope) {
-      var initRequest;
-      initRequest = function() {
-        $rootScope.$broadcast('routesLoaded');
-        return console.log('loading');
-      };
-      initRequest();
-      return OC.Router.registerLoadedCallback(initRequest);
-    }
-  ]);
-
 }).call(this);
 
 
@@ -145,7 +133,7 @@
         this.publisher = publisher;
         this.initialized = false;
         this.shelvedRequests = [];
-        this.$rootScope.$on('routesLoaded', function() {
+        OC.Router.registerLoadedCallback(function() {
           var req, _i, _len, _ref;
           _ref = _this.shelvedRequests;
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
@@ -180,7 +168,7 @@
         postData = $.param(data);
         headers = {
           headers: {
-            'requesttoken': requestToken,
+            'requesttoken': oc_requesttoken,
             'Content-Type': 'application/x-www-form-urlencoded'
           }
         };
@@ -207,170 +195,6 @@
 
     })();
     return Request;
-  });
-
-}).call(this);
-
-
-
-/*
-# ownCloud
-#
-# @author Bernhard Posselt
-# Copyright (c) 2012 - Bernhard Posselt <nukeawhale@gmail.com>
-#
-# This file is licensed under the Affero General Public License version 3 or later.
-# See the COPYING-README file
-#
-*/
-
-
-(function() {
-
-  angular.module('OC').factory('_Model', function() {
-    var Model;
-    Model = (function() {
-
-      function Model() {
-        this.foreignKeys = {};
-        this.data = [];
-        this.ids = {};
-      }
-
-      Model.prototype.handle = function(data) {
-        var item, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _ref2, _results;
-        if (data['create'] !== void 0) {
-          _ref = data['create'];
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            item = _ref[_i];
-            this.create(item);
-          }
-        }
-        if (data['update'] !== void 0) {
-          _ref1 = data['update'];
-          for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-            item = _ref1[_j];
-            this.update(item);
-          }
-        }
-        if (data['delete'] !== void 0) {
-          _ref2 = data['delete'];
-          _results = [];
-          for (_k = 0, _len2 = _ref2.length; _k < _len2; _k++) {
-            item = _ref2[_k];
-            _results.push(this["delete"](item));
-          }
-          return _results;
-        }
-      };
-
-      Model.prototype.hasForeignKey = function(name) {
-        return this.foreignKeys[name] = {};
-      };
-
-      Model.prototype.create = function(data) {
-        var id, ids, name, _base, _ref, _results;
-        if (this.ids[data.id] !== void 0) {
-          return this.update(data);
-        } else {
-          this.data.push(data);
-          this.ids[data.id] = data;
-          _ref = this.foreignKeys;
-          _results = [];
-          for (name in _ref) {
-            ids = _ref[name];
-            id = data[name];
-            (_base = this.foreignKeys[name])[id] || (_base[id] = []);
-            _results.push(this.foreignKeys[name][id].push(data));
-          }
-          return _results;
-        }
-      };
-
-      Model.prototype.update = function(item) {
-        var currentItem, key, value, _results;
-        currentItem = this.ids[item.id];
-        _results = [];
-        for (key in item) {
-          value = item[key];
-          if (this.foreignKeys[key] !== void 0) {
-            if (value !== currentItem[key]) {
-              this._updateForeignKeyCache(key, currentItem, item);
-            }
-          }
-          if (key !== 'id') {
-            _results.push(currentItem[key] = value);
-          } else {
-            _results.push(void 0);
-          }
-        }
-        return _results;
-      };
-
-      Model.prototype["delete"] = function(item) {
-        if (this.getById(item.id) !== void 0) {
-          return this.removeById(item.id);
-        }
-      };
-
-      Model.prototype._updateForeignKeyCache = function(name, currentItem, toItem) {
-        var foreignKeyItems, fromValue, toValue;
-        fromValue = currentItem[name];
-        toValue = toItem[name];
-        foreignKeyItems = this.foreignKeys[name][fromValue];
-        this._removeForeignKeyCacheItem(foreignKeyItems, currentItem);
-        return this.foreignKeys[name][toValue].push(item);
-      };
-
-      Model.prototype._removeForeignKeyCacheItem = function(foreignKeyItems, item) {
-        var fkItem, index, _i, _len, _results;
-        _results = [];
-        for (index = _i = 0, _len = foreignKeyItems.length; _i < _len; index = ++_i) {
-          fkItem = foreignKeyItems[index];
-          if (fkItem.id === id) {
-            _results.push(this.foreignKeys[key][item[key]].splice(index, 1));
-          } else {
-            _results.push(void 0);
-          }
-        }
-        return _results;
-      };
-
-      Model.prototype.removeById = function(id) {
-        var foreignKeyItems, ids, index, item, key, _i, _len, _ref, _ref1;
-        item = this.getById(id);
-        _ref = this.foreignKeys;
-        for (key in _ref) {
-          ids = _ref[key];
-          foreignKeyItems = ids[item[key]];
-          this._removeForeignKeyCacheItem(foreignKeyItems, item);
-        }
-        _ref1 = this.data;
-        for (index = _i = 0, _len = _ref1.length; _i < _len; index = ++_i) {
-          item = _ref1[index];
-          if (item.id === id) {
-            this.data.splice(index, 1);
-          }
-        }
-        return delete this.ids[id];
-      };
-
-      Model.prototype.getById = function(id) {
-        return this.ids[id];
-      };
-
-      Model.prototype.getAll = function() {
-        return this.data;
-      };
-
-      Model.prototype.getAllOfForeignKeyWithId = function(foreignKeyName, foreignKeyId) {
-        return this.foreignKeys[foreignKeyName][foreignKeyId];
-      };
-
-      return Model;
-
-    })();
-    return Model;
   });
 
 }).call(this);
@@ -426,7 +250,7 @@
   ]);
 
   angular.module('AppTemplateAdvanced').factory('ItemModel', [
-    '_ItemModel', 'Publisher', function(_ItemModel, Publisher) {
+    '_ItemModel', function(_ItemModel) {
       return new _ItemModel();
     }
   ]);
@@ -482,8 +306,7 @@
         AppTemplateAdvancedRequest.prototype.getName = function(route, scope) {
           var success;
           success = function(data) {
-            scope.name = data.data.somesetting;
-            return console.log(data);
+            return scope.name = data.data.somesetting;
           };
           return this.post(route, {}, {}, success);
         };
