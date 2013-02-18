0910b536076a050b0b5e5d2d23ea01fda7eb7942
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-02-19 00:04:59 +0100

    [tx-robot] updated from transifex

--- updater/l10n/tr.php
+++ updater/l10n/tr.php
@@ -1,4 +1,6 @@
 <?php $TRANSLATIONS = array(
+"In progress..." => "Devam ediyor...",
+"Done. Reload the page to proceed." => "Tamamlandı. Devam etmek için sayfayı yenile.",
 "Error" => "Hata",
 "Update" => "Güncelleme"
 );
