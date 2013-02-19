cae6a302e488b951da56ed56fc593a48f0bf2f89
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-02-20 00:02:34 +0100

    [tx-robot] updated from transifex

--- firstrunwizard/l10n/fa.php
+++ firstrunwizard/l10n/fa.php
@@ -1,4 +1,3 @@
 <?php $TRANSLATIONS = array(
-"First Run Wizard" => "راهبری کمکی اجرای اول",
-"Show First Run Wizard again" => "راهبری کمکی اجرای اول را دوباره نمایش بده"
+"Welcome to ownCloud" => "خوش آمدید"
 );
