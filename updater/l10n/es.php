06cc85fa0db6a89a4154e0e81bf54a0c23bd79b9
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-01 00:04:24 +0100

    [tx-robot] updated from transifex

--- updater/l10n/es.php
+++ updater/l10n/es.php
@@ -1,5 +1,7 @@
 <?php $TRANSLATIONS = array(
 "In progress..." => "En proceso...",
+"Done." => "Hecho.",
+"Here is your backup: " => "Aquí está tu copia de seguridad:",
 "Error" => "Error",
 "Update" => "Actualizar"
 );
--- updater/l10n/lv.php
+++ updater/l10n/lv.php
@@ -1,5 +1,7 @@
 <?php $TRANSLATIONS = array(
 "In progress..." => "Notiek...",
+"Done." => "Pabeigts.",
+"Here is your backup: " => "Šeit ir jūsu dublējums:",
 "Error" => "Kļūda",
 "Update" => "Atjaunināt"
 );
