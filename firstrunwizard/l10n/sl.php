8753396eb9842c310fc709485d22a09e8ebeb6f4
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-04 00:04:58 +0100

    [tx-robot] updated from transifex

---
+++ firstrunwizard/l10n/sl.php
@@ -0,0 +1,8 @@
+<?php $TRANSLATIONS = array(
+"Welcome to ownCloud" => "Dobrodošli v sistem ownCloud",
+"Your personal web services. All your files, contacts, calendar and more, in one place." => "Osebna spletna storitev. Vse datoteke, stiki, koledar in še več na enem mestu.",
+"Get the apps to sync your files" => "Pridobi programe za usklajevanje datotek",
+"Connect your desktop apps to ownCloud" => "Poveži namizne programe z ownCloud",
+"Connect your Calendar" => "Poveži koledar",
+"Connect your Contacts" => "Poveži stike"
+);
