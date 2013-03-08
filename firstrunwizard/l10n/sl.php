86476d71e6bfcae2cb39eb788ffefa84f113dd2f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-03-09 00:04:32 +0100

    [tx-robot] updated from transifex

--- firstrunwizard/l10n/sl.php
+++ firstrunwizard/l10n/sl.php
@@ -4,5 +4,8 @@
 "Get the apps to sync your files" => "Pridobi programe za usklajevanje datotek",
 "Connect your desktop apps to ownCloud" => "Poveži namizne programe z ownCloud",
 "Connect your Calendar" => "Poveži koledar",
-"Connect your Contacts" => "Poveži stike"
+"Connect your Contacts" => "Poveži stike",
+"Access files via WebDAV" => "Dostop do datotek preko WebDAV",
+"There’s more information in the <a href=\"http://doc.owncloud.org/server/5.0/user_manual/\">documentation</a> and on our <a href=\"http://owncloud.org\">website</a>." => "Več podrobnosti je mogoče najti v <a href=\"http://doc.owncloud.org/server/5.0/user_manual/\">documentaciji</a> in na spletni strani <a href=\"http://owncloud.org\">ownCloud</a>.",
+"If you like ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">recommend it to your friends</a>!" => "Če vam je oblak ownCloud všeč, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">ga priporočite prijateljem</a>!"
 );
--- gallery/l10n/sl.php
+++ gallery/l10n/sl.php
@@ -1,5 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Slike",
+"Shared by" => "Razvrščeno po",
 "Share" => "Souporaba",
 "The \"Image Viewer\" application also need to be enabled to use this application." => "Za uporabo tega programa mora biti omogočen tudi program \"Pregledovalnik slik\"."
 );
