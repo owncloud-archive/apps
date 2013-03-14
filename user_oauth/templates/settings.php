e2c994326bff5bbb22035dfa6af791b0e44fed92
François Kooman <fkooman@tuxed.net>
2013-03-14 11:59:35 +0100

    source formatting

--- user_oauth/templates/settings.php
+++ user_oauth/templates/settings.php
@@ -1,8 +1,8 @@
 <form id="user_oauth">
-	<fieldset class="personalblock">
-		<strong>OAuth</strong><br />
-		<input type="text" name="tokenInfoEndpoint" id="tokenInfoEndpoint" value="<?php p($_['tokenInfoEndpoint']); ?>" placeholder="<?php p($l->t('Token Info Endpoint'));?>" />
-		<br />
-		<span class="msg">Provide the OAuth Authorization Server Token Info Endpoint here.</span>
-	</fieldset>
+    <fieldset class="personalblock">
+        <strong>OAuth</strong><br />
+        <input type="text" name="tokenInfoEndpoint" id="tokenInfoEndpoint" value="<?php p($_['tokenInfoEndpoint']); ?>" placeholder="<?php p($l->t('Token Info Endpoint'));?>" />
+        <br />
+        <span class="msg">Provide the OAuth Authorization Server Token Info Endpoint here.</span>
+    </fieldset>
 </form>
