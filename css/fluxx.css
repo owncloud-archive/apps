49ec34a2dccda4cb51a63fbd699ba26e1d588fac
Daniel Molkentin <daniel@molkentin.de>
2013-03-24 18:12:46 +0100

    Remove old repo link

--- css/fluxx.css
+++ css/fluxx.css
@@ -5,7 +5,6 @@
 * @copyright 2012-2013 Christian Reiner <foss@christian-reiner.info>
 * @license GNU Affero General Public license (AGPL)
 * @link information http://apps.owncloud.com/content/show.php?content=157091
-* @link repository https://svn.christian-reiner.info/svn/app/oc/fluxx-compensator
 *
 * This library is free software; you can redistribute it and/or
 * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
