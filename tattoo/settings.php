6f6c4acc08759c63d206a7f6348e0e4bf1a87cac
Luc Didry <luc@didry.org>
2013-03-24 10:59:40 +0100

    Use OCP namespace

--- tattoo/settings.php
+++ tattoo/settings.php
@@ -1,15 +1,15 @@
 <?php
 
-OC_Util::addStyle('tattoo', 'settings');
+OCP\Util::addStyle('tattoo', 'settings');
 
 // die($_POST['tattooWallpaper']);
 if(isset($_POST['tattooSetWallpaper']) && isset($_POST['tattooWallpaper'])) {
-	OC_Preferences::setValue(OC_User::getUser(),'tattoo','wallpaper',$_POST['tattooWallpaper']);
-	OC_Preferences::setValue(OC_User::getUser(),'tattoo','lastModified',gmdate('D, d M Y H:i:s') . ' GMT');
+	OCP\Config::setUserValue(OCP\User::getUser(),'tattoo','wallpaper',$_POST['tattooWallpaper']);
+	OCP\Config::setUserValue(OCP\User::getUser(),'tattoo','lastModified',gmdate('D, d M Y H:i:s') . ' GMT');
 }
-$wallpaper=OC_Preferences::getValue(OC_User::getUser(),'tattoo','wallpaper','none');
+$wallpaper=OCP\Config::getUserValue(OCP\User::getUser(),'tattoo','wallpaper','none');
 
-$tmpl = new OC_Template( 'tattoo', 'settings');
+$tmpl = new OCP\Template( 'tattoo', 'settings');
 $tmpl->assign('tattooSelectedWallpaper',$wallpaper);
 
 return $tmpl->fetchPage();
