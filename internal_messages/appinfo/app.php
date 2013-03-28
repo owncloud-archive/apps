827d515dace9cbe3933b739e02e3b136a2f6f393
Tharindu <tdgalappaththi@gmail.com>
2013-03-28 13:39:21 +0530

    Modified Internal messages app

---
+++ internal_messages/appinfo/app.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'apps/internal_messages/lib/internalmessages.php';
+
+OCP\Util::addscript('internal_messages','messages');
+OCP\Util::addStyle ('internal_messages','style');
+
+$unread = OC_INT_MESSAGES::unreadMessages( OCP\USER::getUser() );
+if ($unread) { $name = "Messages<strong id=unread_count>(".$unread.")</strong>" ; } else { $name = "Messages" ; }
+
+OCP\App::addNavigationEntry(
+    array( 'id' => 'internal_messages_index',
+           'order' => 74,
+           'href' => OCP\Util::linkTo( 'internal_messages' , 'index.php' ),
+           'icon' => OCP\Util::imagePath( 'internal_messages', 'message.png' ),
+           'name' => $name  )
+   );
