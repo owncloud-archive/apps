285a48c77f41c9aa3e22357a6df819524773cfe7
Tom Needham <tom@owncloud.com>
2013-03-28 20:39:03 +0000

    Files_texteditor: Don't edit .rtf files

--- files_texteditor/js/editor.js
+++ files_texteditor/js/editor.js
@@ -183,72 +183,78 @@ function giveEditorFocus() {
 
 // Loads the file editor. Accepts two parameters, dir and filename.
 function showFileEditor(dir, filename) {
-	if (!editorIsShown()) {
-		is_editor_shown = true;
-		// Delete any old editors
-		$('#editor').remove();
-		// Loads the file editor and display it.
-		$('#content').append('<div id="editor"></div>');
-		var data = $.getJSON(
-			OC.filePath('files_texteditor', 'ajax', 'loadfile.php'),
-			{file: filename, dir: dir},
-			function (result) {
-				if (result.status == 'success') {
-					// Save mtime
-					$('#editor').attr('data-mtime', result.data.mtime);
-					// Initialise the editor
-					$('.actions,#file_action_panel,#content table').hide();
-					// Show the control bar
-					showControls(dir, filename, result.data.writeable);
-					// Update document title
-					$('body').attr('old_title', document.title);
-					document.title = filename + ' - ownCloud';
-					$('#editor').text(result.data.filecontents);
-					$('#editor').attr('data-dir', dir);
-					$('#editor').attr('data-filename', filename);
-					$('#editor').attr('data-edited', 'false');
-					window.aceEditor = ace.edit("editor");
-					aceEditor.setShowPrintMargin(false);
-					aceEditor.getSession().setUseWrapMode(true);
-					if ( ! result.data.writeable ) {
-						aceEditor.setReadOnly(true);
-					}
-					setEditorSize();
-					if (result.data.mime && result.data.mime === 'text/html') {
-						setSyntaxMode('html');
+	// Check if unsupported file format
+	if(FileActions.getCurrentMimeType() == 'text/rtf') {
+		// Download the file instead.
+		window.location = OC.filePath('files', 'ajax', 'download.php') + '?files=' + encodeURIComponent(filename) + '&dir=' + encodeURIComponent($('#dir').val());
+	} else {
+		if (!editorIsShown()) {
+			is_editor_shown = true;
+			// Delete any old editors
+			$('#editor').remove();
+			// Loads the file editor and display it.
+			$('#content').append('<div id="editor"></div>');
+			var data = $.getJSON(
+				OC.filePath('files_texteditor', 'ajax', 'loadfile.php'),
+				{file: filename, dir: dir},
+				function (result) {
+					if (result.status == 'success') {
+						// Save mtime
+						$('#editor').attr('data-mtime', result.data.mtime);
+						// Initialise the editor
+						$('.actions,#file_action_panel,#content table').hide();
+						// Show the control bar
+						showControls(dir, filename, result.data.writeable);
+						// Update document title
+						$('body').attr('old_title', document.title);
+						document.title = filename + ' - ownCloud';
+						$('#editor').text(result.data.filecontents);
+						$('#editor').attr('data-dir', dir);
+						$('#editor').attr('data-filename', filename);
+						$('#editor').attr('data-edited', 'false');
+						window.aceEditor = ace.edit("editor");
+						aceEditor.setShowPrintMargin(false);
+						aceEditor.getSession().setUseWrapMode(true);
+						if ( ! result.data.writeable ) {
+							aceEditor.setReadOnly(true);
+						}
+						setEditorSize();
+						if (result.data.mime && result.data.mime === 'text/html') {
+							setSyntaxMode('html');
+						} else {
+							setSyntaxMode(getFileExtension(filename));
+						}
+						OC.addScript('files_texteditor', 'aceeditor/theme-clouds', function () {
+							window.aceEditor.setTheme("ace/theme/clouds");
+						});
+						window.aceEditor.getSession().on('change', function () {
+							if ($('#editor').attr('data-edited') != 'true') {
+								$('#editor').attr('data-edited', 'true');
+								$('.crumb.last').text($('.crumb.last').text() + ' *');
+								document.title = $('#editor').attr('data-filename') + ' * - ownCloud';
+							}
+						});
+						// Add the ctrl+s event
+						window.aceEditor.commands.addCommand({
+							name: "save",
+							bindKey: {
+								win: "Ctrl-S",
+								mac: "Command-S",
+								sender: "editor"
+							},
+							exec: function () {
+								doFileSave();
+							}
+						});
 					} else {
-						setSyntaxMode(getFileExtension(filename));
+						// Failed to get the file.
+						OC.dialogs.alert(result.data.message, t('files_texteditor', 'An error occurred!'));
 					}
-					OC.addScript('files_texteditor', 'aceeditor/theme-clouds', function () {
-						window.aceEditor.setTheme("ace/theme/clouds");
-					});
-					window.aceEditor.getSession().on('change', function () {
-						if ($('#editor').attr('data-edited') != 'true') {
-							$('#editor').attr('data-edited', 'true');
-							$('.crumb.last').text($('.crumb.last').text() + ' *');
-							document.title = $('#editor').attr('data-filename') + ' * - ownCloud';
-						}
-					});
-					// Add the ctrl+s event
-					window.aceEditor.commands.addCommand({
-						name: "save",
-						bindKey: {
-							win: "Ctrl-S",
-							mac: "Command-S",
-							sender: "editor"
-						},
-						exec: function () {
-							doFileSave();
-						}
-					});
-				} else {
-					// Failed to get the file.
-					OC.dialogs.alert(result.data.message, t('files_texteditor', 'An error occurred!'));
+					// End success
 				}
-				// End success
-			}
-			// End ajax
-		);
+				// End ajax
+			);
+		}
 	}
 }
 
