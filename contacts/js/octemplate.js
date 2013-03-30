b1afa24f1fb184b9f706266a4e330b09575e17e2
Thomas Tanghus <thomas@tanghus.net>
2013-03-30 07:31:46 +0100

    Contacts: Cleaup template plugin

--- contacts/js/octemplate.js
+++ contacts/js/octemplate.js
@@ -1,10 +1,4 @@
 (function( $ ) {
-	// Support older browsers. From http://www.yelotofu.com/2008/08/jquery-outerhtml/
-	$.fn.outerHTML = function(s) {
-		return s
-			? this.before(s).remove()
-			: $('<p>').append(this.eq(0).clone()).html();
-	};
 	/**
 	* Object Template
 	* Inspired by micro templating done by e.g. underscore.js
@@ -15,18 +9,15 @@
 			this.vars = vars;
 			this.options = $.extend({},this.options,options);
 
-			// Save the element reference, both as a jQuery
-			// reference and a normal reference
-			this.elem  = elem;
 			this.$elem = $(elem);
 			var self = this;
 
 			if(typeof this.options.escapeFunction === 'function') {
-			$.each(this.vars, function(key, val) {
-				if(typeof val === 'string') {
-					self.vars[key] = self.options.escapeFunction(val);
-				}
-			});
+				$.each(this.vars, function(key, val) {
+					if(typeof val === 'string') {
+						self.vars[key] = self.options.escapeFunction(val);
+					}
+				});
 			}
 
 			var _html = this._build(this.vars);
@@ -35,8 +26,6 @@
 		// From stackoverflow.com/questions/1408289/best-way-to-do-variable-interpolation-in-javascript
 		_build: function(o){
 			var data = this.$elem.html();
-				//this.$elem.attr('type') === 'text/template'
-				//? this.$elem.html() : this.$elem.outerHTML();
 			return data.replace(/{([^{}]*)}/g,
 				function (a, b) {
 					var r = o[b];
@@ -45,12 +34,13 @@
 			);
 		},
 		options: {
-			escapeFunction: escapeHTML
+			escapeFunction: function(str) {return $('<i></i>').text(str).html();}
 		}
 	};
 
 	$.fn.octemplate = function(vars, options) {
-		if ( this.length ) {
+		var vars = vars ? vars : {};
+		if(this.length) {
 			var _template = Object.create(Template);
 			return _template.init(vars, options, this);
 		}
