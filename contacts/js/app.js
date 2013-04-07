6e092bcda4a0d7f54ae381181327e10aa7b59442
Thomas Tanghus <thomas@tanghus.net>
2013-04-07 04:56:54 +0200

    Contacts: Removed js compatibility functions now in core.

--- contacts/js/app.js
+++ contacts/js/app.js
@@ -6,30 +6,6 @@ Modernizr.load({
 		]
 });
 
-if (typeof Object.create !== 'function') {
-	Object.create = function (o) {
-		function F() {}
-		F.prototype = o;
-		return new F();
-	};
-}
-
-
-if (typeof Object.keys !== 'function') {
-	Object.keys = function(o) {
-		if (o !== Object(o)) {
-			throw new TypeError('Object.keys called on a non-object');
-		}
-		var k=[],p;
-		for (p in o) {
-			if (Object.prototype.hasOwnProperty.call(o,p)) {
-				k.push(p);
-			}
-		}
-		return k;
-	}
-}
-
 (function($) {
 	$.QueryString = (function(a) {
 		if (a == "") return {};
