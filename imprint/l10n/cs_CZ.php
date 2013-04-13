8600c881762d3466247b49de3408c6862ed8a6c0
doktornotor <jakub.moc@gmail.com>
2013-04-13 14:13:30 +0300

    Add Czech translation

--- imprint/l10n/cs_CZ.php
+++ imprint/l10n/cs_CZ.php
@@ -1,4 +1,16 @@
 <?php $TRANSLATIONS = array(
+"Legal notice" => "Právní informace",
+"Nothing here yet" => "Zde ještě nic není",
+"The content of the legal notice has to be configured first" => "Nejprve je nutno nastavit obsah právní informace",
+"The configuration is done here" => "Zde se nacházejí nastavení",
 "Admin" => "Administrace",
-"Content" => "Obsah"
+"Imprint" => "Tiráž",
+"Placement" => "Umístění",
+"Standalone app" => "Samostatná aplikace",
+"Header left" => "Záhlaví vlevo",
+"Header right" => "Záhlaví vpravo",
+"Navigation top" => "Navigace nahoře",
+"Navigation bottom" => "Navigace dole",
+"Content" => "Obsah",
+"You can use html markup (e.g. &lt;br&gt; for a linebreak) and inline style attributes (e.g. &lt;a style=\"color:red;\"&gt;)." => "Můžete využít HTML značky (např. &lt;br&gt; pro odřádkování) a inline atributy stylů (např. &lt;a style=\"color:red;\"&gt;)."
 );
