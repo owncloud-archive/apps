714f72a1823a86203844e2d8f80078acbfbaa472
Lloyd <lloydharis@gmail.com>
2013-04-19 16:16:43 +1000

    retrive html content from file and show

--- file_previewer/docViewer.php
+++ file_previewer/docViewer.php
@@ -8,6 +8,14 @@ $file = isset($_GET['file']) ? $_GET['file'] : '';
 $doc_root = $_SERVER["DOCUMENT_ROOT"];
 $user = OCP\User::getUser();
 
+$path_parts = pathinfo($file);
+$file_name = basename($file, '.'.$path_parts['extension']);
+
 $mime = "application/msword";
 
-$mystring = system('python /opt/jischtml5/tools/commandline/WordDownOO.py '.$doc_root.'/owncloud/data/'.$user.'/files'.$dir.$file , $retval);
+//how to do caching? well see if this id or something in the cache and if not, insert it and then show.
+//otherwise, retrieve it from cache and show
+
+system('python /opt/jischtml5/tools/commandline/WordDownOO.py '.$doc_root.'/owncloud/data/'.$user.'/files'.$dir.$file , $retval);
+$content = file_get_contents($doc_root.'/owncloud/data/'.$user.'/files/'.$file_name.'/'.$file_name.'.html');
+print $content;
