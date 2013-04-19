e9c879cd1367713c12d0c1ae0f4da31c209bf104
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-04-20 00:49:43 +0200

    [tx-robot] updated from transifex

--- imprint/l10n/es_AR.php
+++ imprint/l10n/es_AR.php
@@ -2,7 +2,15 @@
 "Legal notice" => "Información Legal",
 "Nothing here yet" => "Todavía no hay nada aquí",
 "The content of the legal notice has to be configured first" => "El contenido acerca de la información legal tiene que ser configurado primero",
+"That configuration is done in the administration section." => "Eso se configura en la sección de administración.",
+"That configuration has to be done by the system administration." => "Eso tiene que configurarse en administración de sistema.",
+"Imprint" => "Imprint",
+"Placement" => "Ubucación",
+"Standalone app" => "Aplicación específica",
 "Header left" => "Encabezado izquierdo",
 "Header right" => "Encabezado derecho",
-"Content" => "Contenido"
+"Navigation top" => "Navegación extremo superior",
+"Navigation bottom" => "Navegación extremo inferior",
+"Content" => "Contenido",
+"You can use html markup (e.g. &lt;br&gt; for a linebreak) and inline style attributes (e.g. &lt;a style=\"color:red;\"&gt;)." => "Podés usar código html (p.ej. &lt;br&gt; para un salto de línea) y atributos de estilo (p.ej.  &lt;a style=\"color:red;\"&gt;)."
 );
