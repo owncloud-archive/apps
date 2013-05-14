5d152a0740f41fc8255b7ee5955a37afe5f60be4
Evil2000 <david.schueler@schueler.homeip.net>
2013-05-14 19:19:14 +0300

    Fix unallowed chars in XML. Added braces.

--- media/lib/ampache.php
+++ media/lib/ampache.php
@@ -317,7 +317,9 @@ function get_xml_entity_at_index_0($CHAR) {
 }
 
 function numeric_entity_4_char($char) {
-	if (ord($char) < 32) return "";
+	if (ord($char) < 32) {
+		return "";
+	}
 	return "&#" . str_pad(ord($char), 3, '0', STR_PAD_LEFT) . ";";
 }
 
