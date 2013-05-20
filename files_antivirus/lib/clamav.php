7df3f694112e4260a5284c5dc32caa02e50903a8
Thomas Mueller <thomas.mueller@tmit.eu>
2013-05-20 14:05:53 +0200

    closes #1118

--- files_antivirus/lib/clamav.php
+++ files_antivirus/lib/clamav.php
@@ -105,7 +105,7 @@ class OC_Files_Antivirus {
 		}
 		fwrite($shandler, pack('N', 0));
 		$response = fgets($shandler);
-		\OCP\Util::writeLog('files_antivirus', 'Response :: '.$response, \OCP\Util::WARN);
+		\OCP\Util::writeLog('files_antivirus', 'Response :: '.$response, \OCP\Util::DEBUG);
 		fclose($shandler);
 		fclose($fhandler);
 
