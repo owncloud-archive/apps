bbd476f77c6cb20076ed44fb480cd3d7e5564837
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-05-21 13:04:36 +0200

    make search lucene comply with Filesystem / Mount changes

--- search_lucene/lib/indexer.php
+++ search_lucene/lib/indexer.php
@@ -135,9 +135,9 @@ class Indexer {
 			return true;
 
 		} else {
-            Util::writeLog('search_lucene',
+			Util::writeLog('search_lucene',
 				'need mimetype for content extraction',
-                Util::ERROR);
+				Util::ERROR);
 			return false;
 		}
 	}
@@ -217,9 +217,9 @@ class Indexer {
 		}
 
 		if (isset($data['error'])) {
-            Util::writeLog('search_lucene',
+			Util::writeLog('search_lucene',
 				'failed to extract meta information for ' . $view->getAbsolutePath($path) . ': ' . $data['error']['0'],
-                Util::WARN);
+				Util::WARN);
 
 			return;
 		}
@@ -233,8 +233,8 @@ class Indexer {
 	static public function getUnindexed() {
 		$files = array();
 		$absoluteRoot = Filesystem::getView()->getAbsolutePath('/');
-		$mounts = \OC\Files\Mount::findIn($absoluteRoot);
-		$mount = \OC\Files\Mount::find($absoluteRoot);
+		$mounts = Filesystem::getMountPoints($absoluteRoot);
+		$mount = Filesystem::getMountPoint($absoluteRoot);
 		if (!in_array($mount, $mounts)) {
 			$mounts[] = $mount;
 		}
@@ -247,7 +247,16 @@ class Indexer {
 			. ' AND `status` is null OR `status` = "N"');
 
 		foreach ($mounts as $mount) {
-			$storage = $mount->getStorage();
+			if (is_string($mount)) {
+				$storage = Filesystem::getStorage($mount);
+			} else if ($mount instanceof \OC\Files\Mount\Mount) {
+				$storage = $mount->getStorage();
+			} else {
+				$storage = null;
+				Util::writeLog('search_lucene',
+					'expected string or instance of \OC\Files\Mount\Mount got ' . json_encode($mount),
+					Util::DEBUG);
+			}
 			//only index local files for now
 			if ($storage instanceof \OC\Files\Storage\Local) {
 				$cache = $storage->getCache();
