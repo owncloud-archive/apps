2aece39c29f21f3a6ab8b7f8556de0b49e388dba
Bastien Ho <bastienho@urbancube.fr>
2013-05-22 10:15:06 +0200

    dnld button

--- files_pdfviewer/templates/pdf.php
+++ files_pdfviewer/templates/pdf.php
@@ -108,8 +108,8 @@
                 </button>
 
                 <button id="download" class="toolbarButton download" title="Download" tabindex="15" data-l10n-id="download">
--                  <span data-l10n-id="download_label">Download</span>
--                </button> 
+                  <span data-l10n-id="download_label">Download</span>
+                </button> 
 
                 <!-- <div class="toolbarButtonSpacer"></div> -->
                 <a href="#" id="viewBookmark" class="toolbarButton bookmark hiddenSmallView" title="Current view (copy or open in new window)" tabindex="16" data-l10n-id="bookmark"><span data-l10n-id="bookmark_label">Current View</span></a>
