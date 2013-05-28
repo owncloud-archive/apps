c72d4e58e68d118e220d813c34892477d5c79570
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-05-29 00:55:29 +0200

    [tx-robot] updated from transifex

--- gallery/l10n/et_EE.php
+++ gallery/l10n/et_EE.php
@@ -1,6 +1,7 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Pildid",
 "Shared by" => "Jagas",
+"Error loading slideshow template" => "Viga slaidiseansi kujunduse valimisel",
 "Share" => "Jaga",
 "The \"Image Viewer\" application also need to be enabled to use this application." => "\"Pildivaataja\" rakendus tuleb sisse lülitada, et seda rakendust kasutada."
 );
