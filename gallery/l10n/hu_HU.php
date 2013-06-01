5a7334f57623a9db537fa8a45b816182b4277240
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-06-02 00:56:04 +0200

    [tx-robot] updated from transifex

--- gallery/l10n/hu_HU.php
+++ gallery/l10n/hu_HU.php
@@ -1,6 +1,7 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Képek",
 "Shared by" => "Megosztotta Önnel",
+"Error loading slideshow template" => "Hiba történt a diasor sablonjának betöltésekor",
 "Share" => "Megosztás",
 "The \"Image Viewer\" application also need to be enabled to use this application." => "A képnézegető alkalmazásnak is engedélyezve kell lennie az alkalmazás használatához."
 );
