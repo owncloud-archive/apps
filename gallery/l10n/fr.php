7c98c92f276f4651d67124a665492c094259a23f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-06-06 00:50:11 +0200

    [tx-robot] updated from transifex

--- gallery/l10n/fr.php
+++ gallery/l10n/fr.php
@@ -1,6 +1,7 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Images",
 "Shared by" => "Partager par",
+"Error loading slideshow template" => "Erreur lors du téléchargement de la matrice du diaporama ",
 "Share" => "Partager",
 "The \"Image Viewer\" application also need to be enabled to use this application." => "L'application \"Visualiseur d'Image\" nécessite d'être autorisée à utiliser cette application."
 );
