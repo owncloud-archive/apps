2085484aaa55794f13af8a26a2507817ad8e025f
Björn Schießle <schiessle@owncloud.com>
2013-06-12 16:02:34 +0200

    make first run wizard aware of different ownCloud editions

--- firstrunwizard/templates/wizard.php
+++ firstrunwizard/templates/wizard.php
@@ -5,7 +5,11 @@
 </a>
 
 <h1><?php p($l->t('Welcome to ownCloud'));?></h1>
+<?php if (OC_Util::getEditionString() === ''): ?>
 <p><?php p($l->t('Your personal web services. All your files, contacts, calendar and more, in one place.'));?></p>
+<?php else: ?>
+<p><?php p(OC_Defaults::getSlogan()); ?></p>
+<?php endif; ?>
 
 
 <h2><?php p($l->t('Get the apps to sync your files'));?></h2>
@@ -19,20 +23,26 @@
 	<img src="<?php print_unescaped(OCP\Util::imagePath('core', 'appstore.png')); ?>" />
 </a>
 
-
 <h2><?php p($l->t('Connect your desktop apps to ownCloud'));?></h2>
+<?php if (OC_Util::getEditionString() === ''): ?>
 <a class="button" href="http://doc.owncloud.org/server/5.0/user_manual/calendars.html#synchronising-calendars-with-caldav">
 	<img class="appsmall appsmall-calendar svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/calendar-dark.svg')); ?>" /> <?php p($l->t('Connect your Calendar'));?>
 </a>
 <a class="button" href="http://doc.owncloud.org/server/5.0/user_manual/contacts.html#keeping-your-address-book-in-sync">
 	<img class="appsmall appsmall-contacts svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/contacts-dark.svg')); ?>" /> <?php p($l->t('Connect your Contacts'));?>
 </a>
-<a class="button" href="http://doc.owncloud.org/server/5.0/user_manual/connecting_webdav.html">
+<?php endif; ?>
+<a class="button" href="<?php p(OC_Defaults::getDocBaseUrl()); ?>/server/5.0/user_manual/connecting_webdav.html">
 	<img class="appsmall svg" src="<?php print_unescaped(OCP\Util::imagePath('core', 'places/folder.svg')); ?>" /> <?php p($l->t('Access files via WebDAV'));?>
 </a>
 
-<p class="footnote"><?php print_unescaped($l->t('There’s more information in the <a href="http://doc.owncloud.org/server/5.0/user_manual/">documentation</a> and on our <a href="http://owncloud.org">website</a>.')); ?><br>
+<p class="footnote">
+<?php if (OC_Util::getEditionString() === ''): ?>
+<?php print_unescaped($l->t('There’s more information in the <a href="http://doc.owncloud.org/server/5.0/user_manual/">documentation</a> and on our <a href="http://owncloud.org">website</a>.')); ?><br>
 <?php print_unescaped($l->t('If you like ownCloud, <a href="mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org">recommend it to your friends</a>!')); ?></p>
-
+<?php else: ?>
+© 2013 <a href="https://owncloud.com" target="_blank">ownCloud Inc.</a>
+<?php endif; ?>
+</p>
 
 </div>
