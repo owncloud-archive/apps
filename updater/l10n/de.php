280f0cb3b2d114cc53d761519e2a2713dfa06c0a
Thomas Mueller <thomas.mueller@tmit.eu>
2013-06-21 22:55:26 +0200

    In German we want uppercase 'Du' and 'Deine*'

--- updater/l10n/de.php
+++ updater/l10n/de.php
@@ -1,7 +1,7 @@
 <?php $TRANSLATIONS = array(
 "In progress..." => "In Bearbeitung...",
 "Done." => "Erledigt.",
-"Here is your backup: " => "Hier ist dein Back-Up:",
+"Here is your backup: " => "Hier ist Dein Back-Up:",
 "Error" => "Fehler",
 "Update" => "Update durchführen"
 );
