108dd1fd0366934aab9732940e3ada8a270bbb4a
matze <matze@mdoetsch.de>
2013-06-23 23:01:01 +0200

    image view: prev/next support for all images in current folder

--- files_imageviewer/js/lightbox.js
+++ files_imageviewer/js/lightbox.js
@@ -1,32 +1,25 @@
-$(document).ready(function() {
-	if(typeof FileActions!=='undefined'){
-		FileActions.register('image','View', OC.PERMISSION_READ, '',function(filename){
-			viewImage($('#dir').val(),filename);
-		});
-		FileActions.setDefault('image','View');
+$(function() {
+	if	(typeof FileActions!=='undefined') {
+		FileActions.register('image', 'View', OC.PERMISSION_READ, function() {
+			var link = FileActions.currentFile.find('a.name');
+			link.attr('rel', "image").fancybox({
+				"titleFormat": function() {
+					return link.find('.nametext').text();
+				},
+				"titlePosition": "inside"
+			});
+
+			return '';
+		}, function() {});
 	}
-	OC.search.customResults.Images=function(row,item){
-		var image=item.link.substr(item.link.indexOf('download')+8);
-		var a=row.find('a');
-		a.attr('href','#');
-		a.click(function(){
-			image = decodeURIComponent(image);
-			var pos=image.lastIndexOf('/')
-			var file=image.substr(pos + 1);
-			var dir=image.substr(0,pos);
-			viewImage(dir,file);
+
+	OC.search.customResults.Images = function(row){
+		row.find('a').fancybox({
+			"titleFormat": function() {
+				return this.orig.find('.name').text();
+			},
+			"titlePosition": "inside"
 		});
-	}
+	};
 });
 
-function viewImage(dir, file) {
-	if(file.indexOf('.psd')>0){//can't view those
-		return;
-	}
-	var location = fileDownloadPath(dir, file);
-	$.fancybox({
-		"href": location,
-		"title": file.replace(/</, "&lt;").replace(/>/, "&gt;"),
-		"titlePosition": "inside"
-	});
-}
