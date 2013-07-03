94cff0344a63e1e9ab31ee5af7c24f6c8c85e2d2
Gyula Szabo <gyufi@sztaki.hu>
2013-07-03 12:23:40 +0200

    more attributes from federation

---
+++ user_saml/l10n/hu_HU.php
@@ -0,0 +1,4 @@
+<?php $TRANSLATIONS = array(
+"ownCloud desktop password" => "ownCloud desktop jelszÃ³",
+"This is your password that you can use in desktop ownCloud applications: %s" => "Ez a desktop kliensekhez tartozo kezdo jelszod: %sÅ"
+);
