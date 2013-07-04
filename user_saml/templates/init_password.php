0d132f8d2128b708401518e048ef47afa5ca117b
gyufi-sztaki-hu <gyufi@sztaki.hu>
2013-07-04 09:26:02 +0200

    Update init_password.php
    using p() instead echo.

--- user_saml/templates/init_password.php
+++ user_saml/templates/init_password.php
@@ -1,2 +1,2 @@
 <?php
-echo str_replace('{password}', $_['password'], $l->t('This is your password that you can use in desktop ownCloud applications: {password}'));
+p($l->t('This is your password that you can use in desktop ownCloud applications: %s',array($_['password'])));
