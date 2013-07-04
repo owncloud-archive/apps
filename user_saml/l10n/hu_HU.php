0569fc0122d4b0599cc728ae670623eb8f304f72
Gyula Szabo <gyufi@sztaki.hu>
2013-07-04 11:00:50 +0200

    translation fix

--- user_saml/l10n/hu_HU.php
+++
@@ -1,4 +0,0 @@
-<?php $TRANSLATIONS = array(
-"ownCloud desktop password" => "ownCloud desktop jelszó",
-"This is your password that you can use in desktop ownCloud applications: %s" => "Ez a desktop kliensekhez tartozó kezdő jelszavad: %s"
-);
