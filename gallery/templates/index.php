82f4c7df20cf82008be06a4b3e7eb85fdf17a86d
Louis Traynard <louis.traynard@m4x.org>
2013-07-10 12:56:57 +0200

    Update index.php
    Some people do not have short_open_tag on.

--- gallery/templates/index.php
+++ gallery/templates/index.php
@@ -6,6 +6,6 @@
 		<a class="share" data-item-type="gallery" data-item="" title="<?php p($l->t("Share")); ?>"
 		   data-possible-permissions="31"></a>
 	</span>
-	<? endif; ?>
+	<?php endif; ?>
 </div>
 <div id='gallery' class="hascontrols"></div>
