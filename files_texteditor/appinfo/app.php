3dff82c0346ef6a8a33066d3afa9e82cca0ff472
Björn Schießle <schiessle@owncloud.com>
2013-07-12 16:18:08 +0200

    fix if condition

--- files_texteditor/appinfo/app.php
+++ files_texteditor/appinfo/app.php
@@ -1,7 +1,7 @@
 <?php
 
 // only load text editor if the user is logged in
-if (\OCP\User::isLoggedIn() === false) {
+if (\OCP\User::isLoggedIn()) {
 	OCP\Util::addStyle('files_texteditor', 'DroidSansMono/stylesheet');
 	OCP\Util::addStyle('files_texteditor', 'style');
 	OCP\Util::addscript('files_texteditor', 'editor');
