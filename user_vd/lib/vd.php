3d3e9ccd49acdd33dac985af5f54ea88971993de
Bart Visscher <bartv@thisnet.nl>
2013-07-19 13:13:06 +0200

    Change user_vd to use the OCP functions instead of the core ones

--- user_vd/lib/vd.php
+++ user_vd/lib/vd.php
@@ -10,7 +10,7 @@ class OC_USER_VD extends OC_User_Backend{
 		if($this->userExists($uid)){
 			return false;
 		}else{
-			$query=OC_DB::prepare('INSERT INTO *PREFIX*users_vd (uid,password) VALUES (?,?,?,?)');
+			$query=OCP\DB::prepare('INSERT INTO *PREFIX*users_vd (uid,password) VALUES (?,?,?,?)');
 			$result=$query->execute(array($uid,$password));
 			return $result ? true : false;
 		}
@@ -18,14 +18,14 @@ class OC_USER_VD extends OC_User_Backend{
 
 	public function userExists($uid){
 		$uid=$this->checkUid($uid);
-		$query=OC_DB::prepare('SELECT * FROM *PREFIX*users_vd WHERE LOWER(uid) = LOWER(?)');
+		$query=OCP\DB::prepare('SELECT * FROM *PREFIX*users_vd WHERE LOWER(uid) = LOWER(?)');
 		$result=$query->execute(array($uid));
 		return $result->numRows()>0;
 	}
 
 	public function checkPassword($uid,$password){
 		$uid=$this->checkUid($uid);
-		$query = OC_DB::prepare( 'SELECT `uid`, `password` FROM `*PREFIX*users_vd` WHERE LOWER(`uid`) = LOWER(?)' );
+		$query = OCP\DB::prepare( 'SELECT `uid`, `password` FROM `*PREFIX*users_vd` WHERE LOWER(`uid`) = LOWER(?)' );
 		$result = $query->execute( array( $uid));
 		$row=$result->fetchRow();
 		if($row){
@@ -43,14 +43,14 @@ class OC_USER_VD extends OC_User_Backend{
 		$uid=$this->checkUid($uid);
 		if($this->userExists($uid)){
 			list($user,$domain)=explode('@',$uid);
-			return OC_Config::getValue( "datadirectory", OC::$SERVERROOT."/data" ) . '/' . $domain . '/' . $user;
+			return OCP\Config::getSystemValue( "datadirectory", OC::$SERVERROOT."/data" ) . '/' . $domain . '/' . $user;
 		}else{
 			return false;
 		}
 	}
 	
 	public function getUsers($search = '', $limit = null, $offset = null) {
-		$query = OC_DB::prepare('SELECT `uid` FROM `*PREFIX*users_vd` WHERE LOWER(`uid`) LIKE LOWER(?)',$limit,$offset);
+		$query = OCP\DB::prepare('SELECT `uid` FROM `*PREFIX*users_vd` WHERE LOWER(`uid`) LIKE LOWER(?)',$limit,$offset);
 		$result = $query->execute(array($search.'%'));
 		$users = array();
 		while ($row = $result->fetchRow()) {
@@ -73,7 +73,7 @@ class OC_USER_VD extends OC_User_Backend{
 			$fqdn=$_SERVER['SERVER_NAME'];
 		}
 
-		$query=OC_DB::prepare('SELECT domain FROM *PREFIX*users_vd_domains WHERE LOWER(domain)=LOWER(?) OR LOWER(fqdn)=LOWER(?)');
+		$query=OCP\DB::prepare('SELECT domain FROM *PREFIX*users_vd_domains WHERE LOWER(domain)=LOWER(?) OR LOWER(fqdn)=LOWER(?)');
 		$result=$query->execute(array($fqdn,$fqdn));
 		$row=$result->fetchRow();
 		if($row){
