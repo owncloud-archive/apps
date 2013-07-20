daae9fb742e36acbcc6357b8ea5c17afef9cae30
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-20 00:59:55 -0400

    [tx-robot] updated from transifex

--- updater/l10n/et_EE.php
+++ updater/l10n/et_EE.php
@@ -1,6 +1,25 @@
 <?php $TRANSLATIONS = array(
+"Checking your installation..." => "Paigalduse kontrollimine…",
+"Upgrade is not possible. Make sure that your webserver has write access to the following files and directories:" => "Uuendamine pole võimalik. Palun luba oma veebiserveril kirjutamine järgmistesse failidesse ja kataloogidesse:",
+"Version not found" => "Versiooni ei leitud",
+"Upgrade is not possible. Your webserver has not enough permissions to remove the following directory:" => "Uuendamine pole võimalik. Veebiserveril pole õigusi kustutada järgmisi katalooge:",
+"Update permissions on this directory and its content or remove it manually first." => "Uuenda selle kataloogi ja sisu õigusi või eemalda see käsitsi.",
+"Downloading package..." => "Paki allalaadimine…",
+"Unable to fetch package" => "Paki hankimine nurjus",
+"Creating backup..." => "Varukoopia tegemine…",
 "Here is your backup: " => "Siin on su varukoopia:",
+"Failed to create backup" => "Varukoopia loomine nurjus",
+"Moving files..." => "Failide liigutamine…",
+"All done. Click to the link below to start database upgrade." => "Kõik valmis. Klõpsa andmebaasi uuenduse alistamiseks allolevat linki.",
+"Update failed" => "Uuendamine nurjus",
+"The update was unsuccessful. Please report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "Uuendamine nurjus. Palun teata sellest <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloudi kogukonnale</a>.",
+"Update Center" => "Uuendamis keskus",
+"Backup Management" => "Varukoopiate haldus",
 "Update" => "Uuenda",
+"Backup directory" => "Varukoopia kataloog",
+"Backup" => "Varukoopia",
+"Done on" => "Tehtud",
 "Download" => "Lae alla",
-"Delete" => "Kustuta"
+"Delete" => "Kustuta",
+"No backups found" => "Varukoopiaid ei leitud"
 );
