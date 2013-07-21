0557144b751636b5274b5d3395500f0638863cb2
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-21 00:59:38 -0400

    [tx-robot] updated from transifex

--- updater/l10n/tr.php
+++ updater/l10n/tr.php
@@ -1,5 +1,17 @@
 <?php $TRANSLATIONS = array(
+"Checking your installation..." => "Kurulumunuz kontrol ediliyor...",
+"Upgrade is not possible. Make sure that your webserver has write access to the following files and directories:" => "Güncelleme mümkün değil. Aşağıdaki dosya ve dizinlere web sunucunuzun yazma izni olduğundan emin olun:",
+"Version not found" => "Sürüm bulunamadı",
+"Upgrade is not possible. Your webserver has not enough permissions to remove the following directory:" => "Güncelleme mümkün değil. Eeb sunucunuz aşağıdaki dizini silmek için yeterli yetkiye sahip değil:",
+"Update permissions on this directory and its content or remove it manually first." => "Aşağıdaki dizinin ve içindekilerin izinlerini güncelleyin  veya elle silin.",
+"Downloading package..." => "Paket indiriliyor...",
+"Unable to fetch package" => "Paket çekilemedi",
+"Creating backup..." => "Yedek oluşturuluyor...",
 "Here is your backup: " => "Backup sizin burda ",
+"Failed to create backup" => "Yedek oluşturma başarısız oldu",
+"Moving files..." => "Dosyalar taşınıyor...",
+"All done. Click to the link below to start database upgrade." => "Tamamandı. Veritabanı güncellemesi için aşağıdaki bağlantıya tıklayın.",
+"Update failed" => "Güncelleme başarısız",
 "Update" => "Güncelleme",
 "Download" => "İndir",
 "Delete" => "Sil"
