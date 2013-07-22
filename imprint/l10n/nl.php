266f1e04fce0291eef6d17c941ef58548570a5e7
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-22 00:59:56 -0400

    [tx-robot] updated from transifex

--- imprint/l10n/nl.php
+++ imprint/l10n/nl.php
@@ -11,6 +11,7 @@
 "Header right" => "Kop rechts",
 "Navigation top" => "Navigatie bovenaan",
 "Navigation bottom" => "Navigatie onderaan",
+"During login" => "Tijdens inloggen",
 "Content" => "Inhoud",
 "You can use html markup (e.g. &lt;br&gt; for a linebreak) and inline style attributes (e.g. &lt;a style=\"color:red;\"&gt;)." => "U kunt gebruik maken van html opmaak tekens (bijv. &lt;br&gt; voor een regeleinde) en ingebedde stijlattributen  (bijv. &lt;a style=\"color:red;\"&gt;)."
 );
--- updater/l10n/it.php
+++ updater/l10n/it.php
@@ -1,6 +1,25 @@
 <?php $TRANSLATIONS = array(
+"Checking your installation..." => "Controllo la tua installazione...",
+"Upgrade is not possible. Make sure that your webserver has write access to the following files and directories:" => "Impossibile aggiornare. Assicurati che il tuo webserver abbia l'accesso alla scrittura sui seguenti file e cartelle:",
+"Version not found" => "Versione non trovata",
+"Upgrade is not possible. Your webserver has not enough permissions to remove the following directory:" => "Impossibile aggiornare. Il tuo webserver non ha i permessi necessari per rimuovere le seguenti cartelle:",
+"Update permissions on this directory and its content or remove it manually first." => "Aggiorna i permessi su questa cartella e il suo contenuto oppure rimuovila manualmente.",
+"Downloading package..." => "Scaricamento pacchetto...",
+"Unable to fetch package" => "Impossibile ottenere il pacchetto",
+"Creating backup..." => "Creazione backup...",
 "Here is your backup: " => "Qui è la tua copia di sicurezza:",
+"Failed to create backup" => "Impossibile creare backup",
+"Moving files..." => "Spostamento file...",
+"All done. Click to the link below to start database upgrade." => "Tutte le operazioni completate. Clicca sul link qui sotto per iniziare l'aggiornamento del database.",
+"Update failed" => "Update non riuscito",
+"The update was unsuccessful. Please report this issue to the <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">ownCloud community</a>." => "L'aggiornamento non è riuscito. Segnala il problema alla <a href=\"https://github.com/owncloud/apps/issues\" target=\"_blank\">comunità di ownCloud</a>.",
+"Update Center" => "Centro Aggiornamenti",
+"Backup Management" => "Gestione Backup",
 "Update" => "Aggiorna",
+"Backup directory" => "Cartella Backup",
+"Backup" => "Backup",
+"Done on" => "Completato su",
 "Download" => "Scarica",
-"Delete" => "Elimina"
+"Delete" => "Elimina",
+"No backups found" => "Nessun backup trovato"
 );
