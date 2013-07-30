2b38d2002c4122c2c0b91788be4e484929d9e66d
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-07-30 00:50:39 -0400

    [tx-robot] updated from transifex

--- user_migrate/l10n/pt_PT.php
+++ user_migrate/l10n/pt_PT.php
@@ -1,5 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Export" => "Exportar",
 "Something went wrong while the export file was being generated" => "Algo correu mal ao gerar o ficheiro de exportação",
-"An error has occurred" => "Ocorreu um erro"
+"An error has occurred" => "Ocorreu um erro",
+"Account Export/Import" => "Exportar/Importar Conta"
 );
