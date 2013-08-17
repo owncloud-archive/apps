94b0dab07a9548ba2d6c2a12becee9dbcf617d28
Thomas Müller <thomas.mueller@tmit.eu>
2013-08-17 11:43:37 +0200

    remove ru_RU - fixes #3135

--- firstrunwizard/l10n/ru_RU.php
+++
@@ -1,4 +0,0 @@
-<?php $TRANSLATIONS = array(
-"First Run Wizard" => "Помощник первоначальной настройки",
-"Show First Run Wizard again" => "Вновь показать помощника первоначальной настройки"
-);
--- user_openid_provider/l10n/ru_RU.php
+++
@@ -1,6 +0,0 @@
-<?php $TRANSLATIONS = array(
-"OpenID Provider remembered sites" => "Сохранённые сайты OpenID Провайдеров",
-"Trusted" => "Доверенные",
-"Denied" => "Отклонённые",
-"Remove" => "Удалить"
-);
