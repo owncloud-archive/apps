7b3362cb38dbb15e5e8c0606e80e35da892ae057
kondou <kondou@ts.unde.re>
2013-08-17 16:09:41 +0200

    Remove news app from stable

--- news/l10n/hr.php
+++
@@ -1,9 +0,0 @@
-<?php $TRANSLATIONS = array(
-"New" => "novo",
-"Address" => "Adresa",
-"Add" => "Dodaj",
-"Folder" => "mapa",
-"Share" => "Podijeli",
-"Import" => "Uvezi",
-"Export" => "Izvoz"
-);
--- news/l10n/nn_NO.php
+++
@@ -1,8 +0,0 @@
-<?php $TRANSLATIONS = array(
-"New" => "Ny",
-"Address" => "Adresse",
-"Add" => "Legg til",
-"Folder" => "Mappe",
-"Import" => "Importer",
-"Export" => "Eksporter"
-);
