a41ae7c38c18a4e0c2067915761203b0ccfa22b9
Thomas Müller <thomas.mueller@tmit.eu>
2013-08-27 01:28:44 +0200

    killing zh_CN.GB2321

--- imprint/l10n/zh_CN.GB2312.php
+++
@@ -1,4 +0,0 @@
-<?php $TRANSLATIONS = array(
-"Legal notice" => "法律通告",
-"Nothing here yet" => "这里还没有任何东西"
-);
--- user_openid_provider/l10n/zh_CN.GB2312.php
+++
@@ -1,6 +0,0 @@
-<?php $TRANSLATIONS = array(
-"OpenID Provider remembered sites" => "OpenID 提供方记住的站点",
-"Trusted" => "信任的",
-"Denied" => "拒绝的",
-"Remove" => "移除"
-);
