be1c5db28586f3c5803a53bca8d776db0f4907c6
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-08-30 08:24:38 -0400

    [tx-robot] updated from transifex

---
+++ gallery/l10n/en_GB.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"Pictures" => "Pictures",
+"Shared by" => "Shared by",
+"Error loading slideshow template" => "Error loading slideshow template",
+"Share" => "Share",
+"The \"Image Viewer\" application also need to be enabled to use this application." => "The \"Image Viewer\" application also needs to be enabled to use this application."
+);
