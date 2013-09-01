3970b0b5aaf4e286e7d56c8a18b9ab2427cca4f8
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-09-01 11:43:25 -0400

    [tx-robot] updated from transifex

--- imprint/l10n/ar.php
+++ imprint/l10n/ar.php
@@ -1,3 +1,17 @@
 <?php $TRANSLATIONS = array(
-"Admin" => "المدير"
+"Legal notice" => "إشعار قانوني",
+"Nothing here yet" => "لا شيئ هنا إلى الآن.",
+"The content of the legal notice has to be configured first" => "يجب أن يتم تهيئة محتوى الإشعار القانوني أولا.",
+"That configuration is done in the administration section." => "هذا الإعداد قد تم في القسم الإداري.",
+"That configuration has to be done by the system administration." => "هذا الإعداد يجب أن يتم بواسطة إدارة النظام.",
+"Imprint" => "الدمغة.",
+"Placement" => "وضع",
+"Standalone app" => "تطبيق مستقل",
+"Header left" => "رأس صفحة أيسر",
+"Header right" => "رأس صفحة أيمن",
+"Navigation top" => "تصفح أعلى",
+"Navigation bottom" => "تصفح أسقل",
+"During login" => "خلال تسجيل الدخول",
+"Content" => "محتوى",
+"You can use html markup (e.g. &lt;br&gt; for a linebreak) and inline style attributes (e.g. &lt;a style=\"color:red;\"&gt;)." => "يمكنك استخدام علامة HTML (مثال: lt;br&gt& من أجل فاصل أسطر)  وكذا صفات محاذاة النمط (مثال: lt;a style=\"color:red;\"&gt&)."
 );
