8a5c1ef3e35ee7433843c08d931b69cb21ab5c7c
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-09-24 10:57:07 -0400

    [tx-robot] updated from transifex

--- activity/l10n/fr.php
+++ activity/l10n/fr.php
@@ -5,5 +5,6 @@
 "%s deleted" => "%s supprimé",
 "%s created" => "%s créé",
 "RSS feed" => "Flux RSS",
+"No activities yet. You will see a list of events here when you start to use your %s." => "Aucune activité à afficher. La liste des événements s'affichera ici une fois que vous aurez commencé à utiliser %s.",
 "Today" => "Aujourd'hui"
 );
