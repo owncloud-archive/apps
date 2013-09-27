1bfaaef01151703495efd7f767430d801b78c4de
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-09-26 22:02:16 -0400

    [tx-robot] updated from transifex

--- activity/l10n/ca.php
+++ activity/l10n/ca.php
@@ -1,3 +1,10 @@
 <?php $TRANSLATIONS = array(
+"Activity" => "Activitat",
+"test" => "prova",
+"%s changed" => "%s canviat",
+"%s deleted" => "%s esborrat",
+"%s created" => "%s creat",
+"RSS feed" => "Font RSS",
+"No activities yet. You will see a list of events here when you start to use your %s." => "Encara no hi ha activitats. Aquí veureu una llista de successos quan comenceu a usar el vostre %s.",
 "Today" => "Avui"
 );
