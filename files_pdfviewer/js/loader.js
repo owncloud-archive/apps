9a2491c7fd6f9040282eff843be9e57bae55c166
Vincent Petry <pvince81@owncloud.com>
2013-10-08 17:59:28 +0200

    Fixed PDF viewer layout/container in public mode
    In public mode the PDF viewer was appended below the content/preview.
    This fix pases it into the preview container which is whatever is
    displayed in the middle of the page.
    
    The close button is now only visible when a files list is present
    (public or internal) to go back to.

--- files_pdfviewer/js/loader.js
+++ files_pdfviewer/js/loader.js
@@ -3,21 +3,38 @@ function hidePDFviewer() {
 	$("#controls").show();
 	$("#editor").show();
 	$('#pdframe, #pdfbar').remove();
+	if ($('#isPublic').val()){
+		$('#preview').css({height: null});
+	}
 }
 
 function showPDFviewer(dir,filename){
 	if(!showPDFviewer.shown){
+		var $iframe;
 		$("#editor").hide();
 		$('#content table').hide();
 		$("#controls").hide();
 		var oldcontent = $("#content").html();
 		var viewer = OC.linkTo('files_pdfviewer', 'viewer.php')+'?dir='+encodeURIComponent(dir).replace(/%2F/g, '/')+'&file='+encodeURIComponent(filename.replace('&', '%26'));
-		$("#content").append('<iframe id="pdframe" style="width:100%;height:100%;display:block;" src="'+viewer+'" /><div id="pdfbar"><a id="close" title="Close">X</a></div>');
+		$iframe = $('<iframe id="pdframe" style="width:100%;height:100%;display:block;" src="'+viewer+'" /><div id="pdfbar"><a id="close" title="Close">X</a></div>');
+		if ($('#isPublic').val()){
+			// force the preview to adjust its height
+			$('#preview').append($iframe).css({height: '100%'});
+		}
+		else{
+			$('#content').append($iframe);
+		}
 		$("#pageWidthOption").attr("selected","selected");	
 		$('#pdfbar').css({position:'absolute',top:'5px',right:'5px'});
-		$('#close').css({display:'block',padding:'0 5px',color:'#BBBBBB','font-weight':'900',height:'18px',background:'transparent'}).click(function(){
-			hidePDFviewer();
-		});
+		// if a filelist is present, the PDF viewer can be closed to go back there
+		if ($('#fileList').length){
+			$('#close').css({display:'block',padding:'0 5px',color:'#BBBBBB','font-weight':'900',height:'18px',background:'transparent'}).click(function(){
+				hidePDFviewer();
+			});
+		}
+		else{
+			$('#close').css({display:'none'});
+		}
 	}
 
 }
