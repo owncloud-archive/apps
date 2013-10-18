e1e5b047ba179cb44b762651cd8ccd18decb7d6f
Vincent Petry <pvince81@owncloud.com>
2013-10-18 15:23:28 +0200

    Now using FileList.setViewerMode in embeded apps
    Text editor and ODF viewer now use FileList.setViewerMode() calls to
    make sure the controls bar is restored properly to its previous state
    upon closing.

--- files_odfviewer/js/viewer.js
+++ files_odfviewer/js/viewer.js
@@ -4,41 +4,32 @@ function viewOdf(dir, file) {
     OC.addScript('files_odfviewer','webodf').done(function(){
         var location = fileDownloadPath(dir, file);
 
-        // fade out files menu and add odf menu
-        $('.actions,#file_action_panel').fadeOut('slow').promise().done(function() {
-            // odf action toolbar
-            var odfToolbarHtml =
-                '<div id="odf-toolbar">' +
-                '<button id="odf_close">'+t('files_odfviewer','Close')+
-                '</button></div>';
-            $('#controls').append(odfToolbarHtml);
+		// start viewer mode
+		FileList.setViewerMode(true);
 
-        });
+		// odf action toolbar
+		var odfToolbarHtml =
+			'<div id="odf-toolbar">' +
+			'<button id="odf_close">'+t('files_odfviewer','Close')+
+			'</button></div>';
+		$('#controls').append(odfToolbarHtml);
 
-        // fade out file list and show pdf canvas
-        $('table').fadeOut('slow').promise().done(function(){;
-            var canvashtml = '<div id="odf-canvas"></div>';
-			$('table').after(canvashtml);
-			// in case we are on the public sharing page we shall display the odf into the preview tag
-			$('#preview').html(canvashtml);
+		var canvashtml = '<div id="odf-canvas"></div>';
+		$('table').after(canvashtml);
+		// in case we are on the public sharing page we shall display the odf into the preview tag
+		$('#preview').html(canvashtml);
 
-            var odfelement = document.getElementById("odf-canvas");
-            var odfcanvas = new odf.OdfCanvas(odfelement);
-            odfcanvas.load(location);
-        });
+		var odfelement = document.getElementById("odf-canvas");
+		var odfcanvas = new odf.OdfCanvas(odfelement);
+		odfcanvas.load(location);
     });
 }
 
 function closeOdfViewer(){
-	// Fade out odf-toolbar
-	$('#odf-toolbar').fadeOut('slow');
-	// Fade out editor
-	$('#odf-canvas').fadeOut('slow', function(){
-		$('#odf-toolbar').remove();
-		$('#odf-canvas').remove();
-		$('.actions,#file_access_panel').fadeIn('slow');
-		$('table').fadeIn('slow');	
-	});
+	// Remove odf-toolbar
+	$('#odf-toolbar').remove();
+	$('#odf-canvas').remove();
+	FileList.setViewerMode(false);
 	is_editor_shown = false;
 }
 
