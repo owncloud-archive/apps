4e7eff6f49f62cb0c2ecc2dbee38916d16a1910a
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-11-15 17:35:18 +0100

    loading images from correct storage and prepend path

--- gallery/ajax/image.php
+++ gallery/ajax/image.php
@@ -12,12 +12,10 @@ list($token, $img) = explode('/', $_GET['file'], 2);
 $linkItem = \OCP\Share::getShareByToken($token);
 if (is_array($linkItem) && isset($linkItem['uid_owner'])) {
 	// seems to be a valid share
-	$type = $linkItem['item_type'];
-	$fileSource = $linkItem['file_source'];
-	$shareOwner = $linkItem['uid_owner'];
-	$path = null;
 	$rootLinkItem = \OCP\Share::resolveReShare($linkItem);
 	$owner = $rootLinkItem['uid_owner'];
+	OC_Util::tearDownFS();
+	OC_Util::setupFS($owner);
 } else {
 	OCP\JSON::checkLoggedIn();
 
@@ -30,6 +28,12 @@ if (is_array($linkItem) && isset($linkItem['uid_owner'])) {
 session_write_close();
 
 $ownerView = new \OC\Files\View('/' . $owner . '/files');
+
+if (is_array($linkItem) && isset($linkItem['uid_owner'])) {
+	// prepend path to share
+	$path = $ownerView->getPath($linkItem['file_source']);
+	$img = $path.'/'.$img;
+}
 
 $mime = $ownerView->getMimeType($img);
 list($mimePart,) = explode('/', $mime);
