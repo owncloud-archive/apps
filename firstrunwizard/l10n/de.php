e6d7d8f1efe4624725eb53ade7d11d5871ba6af3
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-11-21 18:16:55 -0500

    [tx-robot] updated from transifex

--- firstrunwizard/l10n/de.php
+++ firstrunwizard/l10n/de.php
@@ -6,5 +6,6 @@
 "Connect your Calendar" => "Verbinde Deine Kalender ",
 "Connect your Contacts" => "Verbinde Deine Kontakte",
 "Access files via WebDAV" => "Greife auf Dateien über WebDAV zu",
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Weitere Informationen findest Du in der <a target=\"_blank\" href=\"%s\">Dokumentation</a> und auf unserer <a target=\"_blank\" href=\"http://owncloud.org\">Webseite</a>.",
 "If you like ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">recommend it to your friends</a>!" => "Wenn Dir ownCloud gefällt, <a href=\"mailto:?subject=ownCloud&body=\"ownCloud ist großartige freie Software zum synchronisieren und teilen Deiner Dateien, du kannst es frei von http://owncloud.org herunterladen\">empfiehl es weiter</a>!"
 );
--- firstrunwizard/l10n/pt_BR.php
+++ firstrunwizard/l10n/pt_BR.php
@@ -6,5 +6,6 @@
 "Connect your Calendar" => "Conecte seu Calendário",
 "Connect your Contacts" => "Conecte seus Contatos",
 "Access files via WebDAV" => "Acesse arquivos via WebDAV",
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Existe mais informação em <a target=\"_blank\" href=\"%s\">documentação</a> e em nosso <a target=\"_blank\" href=\"http://owncloud.org\">website</a>",
 "If you like ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">recommend it to your friends</a>!" => "Se você gosta do ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud é um software livre que sincroniza e compartilha seus arquivos. Você pode obtê-lo gratuitamente em http://owncloud.org\">recomende aos seus amigos</a>!"
 );
