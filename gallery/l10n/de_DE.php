ceea7aeb37d182aaa06b54abd8e0ee55e71d8d6f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-11-27 09:18:27 -0500

    [tx-robot] updated from transifex

--- gallery/l10n/de_DE.php
+++ gallery/l10n/de_DE.php
@@ -1,7 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Bilder",
 "Shared by" => "Geteilt von",
+"Picture view" => "Bildansicht",
 "Error loading slideshow template" => "Fehler beim Laden der Slideshow-Vorlage",
 "Share" => "Teilen",
-"The \"Image Viewer\" application also need to be enabled to use this application." => "Die Anwendung \"Image Viewer\" muss aktiviert sein, damit Sie diese Anwendung benutzen können."
+"The \"Image Viewer\" application also need to be enabled to use this application." => "Die Anwendung \"Image Viewer\" muss aktiviert sein, damit Sie diese Anwendung benutzen können.",
+"%s shared <strong>%s</strong> with you" => "%s teilt <strong>%s</strong> mit Ihnen",
+"File list" => "Dateiliste"
 );
--- gallery/l10n/it.php
+++ gallery/l10n/it.php
@@ -1,7 +1,10 @@
 <?php $TRANSLATIONS = array(
 "Pictures" => "Immagini",
 "Shared by" => "Condiviso da",
+"Picture view" => "Vista immagini",
 "Error loading slideshow template" => "Errore durante il caricamento del modello di presentazione",
 "Share" => "Condividi",
-"The \"Image Viewer\" application also need to be enabled to use this application." => "Per utilizzare questa applicazione deve essere abilitato anche il \"Visualizzatore immagini\"."
+"The \"Image Viewer\" application also need to be enabled to use this application." => "Per utilizzare questa applicazione deve essere abilitato anche il \"Visualizzatore immagini\".",
+"%s shared <strong>%s</strong> with you" => "%s ha condiviso <strong>%s</strong> con te",
+"File list" => "Lista file"
 );
