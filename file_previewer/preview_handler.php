8409b5c187fd402e5b45787d058af3128a5729a1
Lloyd <lloyd@lloyd-TravelMate-P653-M>
2013-11-29 11:41:12 +1100

    #62 - cleanup

--- file_previewer/preview_handler.php
+++ file_previewer/preview_handler.php
@@ -9,27 +9,12 @@ $user = OCP\User::getUser();
 $p_parts = pathinfo($file);
 $basename = $p_parts["basename"]; 
 
-//$filename = str_replace("_html/".$basename, "", $file);
-
 if (\OC\Files\Filesystem::isReadable($file)) {
 	list($storage) = \OC\Files\Filesystem::resolvePath($file);
 	if ($storage instanceof \OC\Files\Storage\Local) {
 		$full_path = \OC\Files\Filesystem::getLocalFile($file);
 		echo file_get_contents($full_path);
 		return;
-		/*$path_parts = pathinfo($file);
-		$preview_path = dirname($full_path).'/_html/'.$basename.'/index.html'; //$preview;
-		if(file_exists($preview_path)){
-			echo file_get_contents($preview_path);
-			return;
-		}
-		else{
-			header("HTTP/1.0 404 Preview Not Found");
-			$tmpl = new OC_Template('', '404', 'guest');
-			$tmpl->assign('file', $name);
-			$tmpl->printPage();
-			return;
-		}*/
 	}
 } elseif (!\OC\Files\Filesystem::file_exists($file)) {
 	header("HTTP/1.0 404 Not Found");
