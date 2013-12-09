2889dd3c19dd384c44d7d5ad8a8e684cfe3fed12
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-12-09 06:39:17 -0500

    [tx-robot] updated from transifex

--- activity/l10n/hu_HU.php
+++ activity/l10n/hu_HU.php
@@ -1,6 +1,8 @@
 <?php $TRANSLATIONS = array(
 "Activity" => "Tevékenységek",
+"No more activities to load." => "Nincs több betöltendő tevékenység.",
 "Loading older activities" => "Korábbi aktivitások betöltése",
 "RSS feed" => "RSS hírcsatorna",
-"No activities yet." => "Nem volt még aktívitás."
+"No activities yet." => "Nem volt még aktívitás.",
+"You will see a list of events here when you start to use your %s." => "Az események itt lesznek láthatóak miután elinditottad a %s szolgáltatásodat "
 );
