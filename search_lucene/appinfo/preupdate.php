47f8ef0ddb4dd379172ea9f24071e92a8a0dd9f9
Jörn Friedrich Dreyer <jfd@butonic.de>
2013-12-11 18:36:54 +0100

    execute delete before schema upgrade

---
+++ search_lucene/appinfo/preupdate.php
@@ -0,0 +1,18 @@
+<?php
+
+if (version_compare($currentVersion, '0.5.2', '<')) {
+	//delete duplicate id entries
+	$stmt = OCP\DB::prepare('
+		DELETE FROM `*PREFIX*lucene_status`
+		WHERE `fileid` IN (
+			SELECT `fileid`
+			FROM (
+				SELECT `fileid`
+				FROM `*PREFIX*lucene_status`
+				GROUP BY `fileid`
+				HAVING count(`status`) > 1
+			) AS `mysqlerr1093hack`
+		)
+	');
+	$stmt->execute();
+}
--- search_lucene/appinfo/update.php
+++ search_lucene/appinfo/update.php
@@ -9,20 +9,3 @@ if (version_compare($currentVersion, '0.5.0', '<')) {
 	$stmt = OCP\DB::prepare('DELETE FROM `*PREFIX*queuedtasks` WHERE `app`=?');
 	$stmt->execute(array('search_lucene'));
 }
-
-if (version_compare($currentVersion, '0.5.2', '<')) {
-	//delete duplicate id entries
-	$stmt = OCP\DB::prepare('
-		DELETE FROM `*PREFIX*lucene_status`
-		WHERE `fileid` IN (
-			SELECT `fileid`
-			FROM (
-				SELECT `fileid`
-				FROM `*PREFIX*lucene_status`
-				GROUP BY `fileid`
-				HAVING count(`status`) > 1
-			) AS `mysqlerr1093hack`
-		)
-	');
-	$stmt->execute();
-}
