04661ce5ddfbb9a02395d774a50d9c7a852e5bce
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-12-13 12:43:18 -0500

    [tx-robot] updated from transifex

--- user_openid/l10n/id.php
+++ user_openid/l10n/id.php
@@ -1,14 +1,14 @@
 <?php $TRANSLATIONS = array(
-"OpenID Changed" => "OpenID telah dirubah",
+"OpenID Changed" => "OpenID telah diubah",
 "Invalid request" => "Permintaan tidak sah",
-"This is an OpenID server endpoint. For more information, see " => "ini adalah titik akhir server OpenID. untuk informasi lebih lanjut, lihat",
-"Identity: <b>" => "identitas: <b>",
+"This is an OpenID server endpoint. For more information, see " => "Ini adalah titik akhir server OpenID. untuk informasi lebih lanjut, lihat",
+"Identity: <b>" => "Identitas: <b>",
 "Realm: <b>" => "Dunia: <b>",
-"User: <b>" => "pengguna: <b>",
-"Login" => "login",
-"Error: <b>No user Selected" => "kesalahan: <b> tidak ada pengguna dipilih",
+"User: <b>" => "Pengguna: <b>",
+"Login" => "Masuk",
+"Error: <b>No user Selected" => "Galat: <b>Tidak ada pengguna yang dipilih",
 "OpenID" => "OpenID",
 "you can authenticate to other sites with this address" => "anda dapat masuk ke situs lain dengan alamat ini",
-"Authorized OpenID provider" => "penyedia OpenID yang sah",
-"Your address at Wordpress, Identi.ca, &hellip;" => "alamat anda di Wordpress, Identi.ca, &hellip;"
+"Authorized OpenID provider" => "Penyedia OpenID yang sah",
+"Your address at Wordpress, Identi.ca, &hellip;" => "Alamat anda di Wordpress, Identi.ca, &hellip;"
 );
