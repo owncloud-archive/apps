89be1cf170d2513042ae427e9f89ae24b7117ed5
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-12-22 00:50:07 -0500

    [tx-robot] updated from transifex

--- files_texteditor/l10n/da.php
+++ files_texteditor/l10n/da.php
@@ -7,5 +7,6 @@
 "Saving..." => "Gemmer...",
 "Failed to save file" => "Kunne ikke gemme filen",
 "An error occurred!" => "Der opstod en fejl!",
+"There are unsaved changes in the text editor" => "Der er ugemte ændringer i tekstbehandleren",
 "There were unsaved changes, click here to go back" => "Ændringer blev ikke gemt - klik her for at gå tilbage"
 );
