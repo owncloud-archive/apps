c7ae20a7bd46f88a0b4b370dfdc359c7dbd9256f
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2013-12-25 00:49:40 -0500

    [tx-robot] updated from transifex

--- activity/l10n/sl.php
+++ activity/l10n/sl.php
@@ -1,8 +1,8 @@
 <?php $TRANSLATIONS = array(
-"Activity" => "Dejavnost",
+"Activity" => "Dejavnosti",
 "No more activities to load." => "Ni več dejavnosti za nalaganje.",
 "Loading older activities" => "Nalaganje starejših dejavnosti.",
 "RSS feed" => "Vir RSS",
-"No activities yet." => "Ni nobenih dejavnosti.",
+"No activities yet." => "Ni še zabeleženih nobenih dejavnosti.",
 "You will see a list of events here when you start to use your %s." => "Tu bo izpisan seznam dogodkov, ko bo %s v uporabi."
 );
