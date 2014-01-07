aef294cad9d5a00999c56627f303b942ec553056
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-07 00:50:05 -0500

    [tx-robot] updated from transifex

--- activity/l10n/de_DE.php
+++ activity/l10n/de_DE.php
@@ -1,6 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Activity" => "Aktivität",
-"No more activities to load." => "Keine weiteren Aktivitäten zum Laden.",
+"No more activities to load." => "Es gibt keine weiteren Aktivitäten zum Laden.",
 "Loading older activities" => "Lade ältere Aktivitäten",
 "RSS feed" => "RSS-Feed",
 "No activities yet." => "Bisher keine Aktivitäten.",
