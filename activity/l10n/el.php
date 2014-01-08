80b30366f32cd033172d0bdeeb7330a643066fd4
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-01-08 00:49:43 -0500

    [tx-robot] updated from transifex

--- activity/l10n/el.php
+++ activity/l10n/el.php
@@ -4,5 +4,5 @@
 "Loading older activities" => "Φόρτωση παλαιότερων δραστηριοτήτων",
 "RSS feed" => "Ροή RSS",
 "No activities yet." => "Καμία δραστηριότητα μέχρι τώρα.",
-"You will see a list of events here when you start to use your %s." => "Θα δείτε εδώ μια λίστα από συμβάντα  όταν ξεκινήσετε να χρησιμοποιείτε το %s σας."
+"You will see a list of events here when you start to use your %s." => "Θα δείτε εδώ μια λίστα από συμβάντα όταν ξεκινήσετε να χρησιμοποιείτε το %s σας."
 );
