a2dbeb437951aa21b9f5e625a97438da53d88ee2
Victor Dubiniuk <victor.dubiniuk@gmail.com>
2014-01-25 16:05:11 +0300

    Open in a new tab

--- updater/templates/admin.php
+++ updater/templates/admin.php
@@ -14,5 +14,5 @@
 
 <fieldset class="personalblock">
 	<h2>Updater</h2>
-	<a href="<?php p(\OCP\Util::linkTo('updater', 'update.php')) ?>"><?php p($l->t('Update Center')) ?></a>
+	<a target="_blank" href="<?php p(\OCP\Util::linkTo('updater', 'update.php')) ?>"><?php p($l->t('Update Center')) ?></a>
 </fieldset>
