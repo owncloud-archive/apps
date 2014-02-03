199c313bc14a630a48037a50300fa0f5466cea04
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-02-03 01:17:29 -0500

    [tx-robot] updated from transifex

--- gallery/l10n/ru.php
+++ gallery/l10n/ru.php
@@ -5,5 +5,6 @@
 "Error loading slideshow template" => "Ошибка при загрузке шаблона слайд-шоу",
 "Share" => "Открыть доступ",
 "The \"Image Viewer\" application also need to be enabled to use this application." => "Для работы этого приложения необходимо включить приложение \"Image Viewer\".",
+"shared by %s" => "Поделиться с %s",
 "File list" => "Список файлов"
 );
