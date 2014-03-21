abcd38ec458f51e0af5a726f67e5775ac9333efe
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-03-21 00:49:34 -0400

    [tx-robot] updated from transifex

--- files_antivirus/l10n/ar.php
+++ files_antivirus/l10n/ar.php
@@ -3,6 +3,16 @@
 "Sorry, but a malware was detected in a file you tried to upload and it had to be deleted." => "وجد برنامج خبيث في الملف الذي تريد ترفيعه, و قد تم حذفه.",
 "This email is a notification from {host}. Please, do not reply." => "هذا البريد الالكتروني من ال {host}. رجاء, لا تحاول الاجابه.",
 "File uploaded: {file}" => "تم ترفيع: {file}",
+"Antivirus Configuration" => "إعدادات مضاد الفيروسات",
+"Mode" => "النمط",
+"Executable" => "تشغيلي",
+"Daemon" => "خفي",
+"Not required in Executable Mode." => "غير ضروري في نمط \"القابل للتشغيل\"",
 "Host" => "المضيف",
+"Address of Antivirus Host." => "عنوان مضيف مضاد الفيروساات",
+"Port" => "المنفذ",
+"Port number of Antivirus Host." => "رقم منفذ مضيف مضاد الفيروسات",
+"Path to clamscan" => "المسار إلى clamscan",
+"Path to clamscan executable." => "المسار إلى برنامج clamscan",
 "Save" => "حفظ"
 );
