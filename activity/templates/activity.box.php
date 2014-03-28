b386ac178d997bdb1b115442ccce21c9057890b3
Joas Schilling <nickvergessen@gmx.de>
2014-03-28 15:32:42 +0100

    Change license header to small version and replace echo with p()
    Fix #1729

--- activity/templates/activity.box.php
+++ activity/templates/activity.box.php
@@ -1,32 +1,15 @@
 <?php
-/**
- * ownCloud - Activity App
- *
- * @author Joas Schilling
- * @copyright 2014 Joas Schilling nickvergessen@gmx.de
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+/* Copyright (c) 2014, Joas Schilling nickvergessen@gmx.de
+ * This file is licensed under the Affero General Public License version 3
+ * or later. See the COPYING-README file. */
 
 /** @var $l OC_L10N */
-/** @var $_ */
+/** @var $_ array */
 ?>
 
 <div class="box">
 	<div class="header">
-		<span class="avatar" data-user="<?php echo $_['user'] ?>"></span>
+		<span class="avatar" data-user="<?php p($_['user']) ?>"></span>
 		<span>
 			<span class="user"><?php p($_['displayName']) ?></span>
 			<span class="activitytime tooltip" title="<?php p($_['formattedDate']) ?>">
@@ -41,7 +24,7 @@
 			<ul class="activitysubject grouped">
 				<?php foreach($_['event']['events'] as $subEvent):?>
 					<li>
-						<?php if ($subEvent['link']): ?><a href="<?php echo $subEvent['link'] ?>"><?php endif ?>
+						<?php if ($subEvent['link']): ?><a href="<?php p($subEvent['link']) ?>"><?php endif ?>
 						<?php p($subEvent['subject']) ?>
 						<?php if ($subEvent['link']): ?></a><?php endif ?>
 					</li>
@@ -56,13 +39,13 @@
 			</ul>
 		<?php endif ?>
 		<?php if (!$_['isGrouped']): ?>
-			<?php if ($_['event']['link']): ?><a href="<?php echo $_['event']['link'] ?>"><?php endif ?>
+			<?php if ($_['event']['link']): ?><a href="<?php p($_['event']['link']) ?>"><?php endif ?>
 			<div class="activitysubject"><?php p($_['event']['subject']) ?></div>
 			<div class="activitymessage"><?php p($_['event']['message']) ?></div>
 		<?php endif ?>
 
 		<?php if ($_['previewImageLink']): ?>
-			<img class="preview" src="<?php echo $_['previewImageLink'] ?>" alt="<?php p($_['event']['message']) ?>"/>
+			<img class="preview" src="<?php p($_['previewImageLink']) ?>" alt="<?php p($_['event']['message']) ?>"/>
 		<?php endif ?>
 
 		<?php if (!$_['isGrouped'] && $_['event']['link']): ?></a><?php endif; ?>
