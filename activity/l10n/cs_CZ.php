412078588d35fcff6fc388d9259ab54c660cd858
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-03-31 00:49:42 -0400

    [tx-robot] updated from transifex

--- activity/l10n/cs_CZ.php
+++ activity/l10n/cs_CZ.php
@@ -1,9 +1,12 @@
 <?php $TRANSLATIONS = array(
+"Your settings have been updated." => "Vaše nastavení bylo aktualizováno.",
 "Activity" => "Aktivita",
 "No more activities to load." => "Žádné další aktivity k načtení.",
 "Loading older activities" => "Načítám starší aktivity",
+"A file or folder has been <strong>shared</strong>" => "Soubor nebo složka byla <strong>nasdílena</strong>",
 "RSS feed" => "RSS kanál",
 "No activities yet." => "Zatím žádné aktivity.",
 "You will see a list of events here when you start to use your %s." => "Zde uvidíte seznam událostí jakmile začnete používat váš %s.",
-"Notifications" => "Upozornění"
+"Notifications" => "Upozornění",
+"Mail" => "Pošta"
 );
