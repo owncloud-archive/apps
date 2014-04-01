da725cd4ecbf9d352f214b1ca009a87fbf67eca8
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-04-01 18:44:56 +0200

    Updater: fix look of settings, change from personalblock

--- updater/templates/admin.php
+++ updater/templates/admin.php
@@ -12,7 +12,7 @@
 
 ?>
 
-<fieldset class="personalblock">
+<div class="section">
 	<h2>Updater</h2>
 	<a target="_blank" href="<?php p(\OCP\Util::linkTo('updater', 'update.php')) ?>"><?php p($l->t('Update Center')) ?></a>
-</fieldset>
+</div>
