c876e7d72f8a8b88ccbf6beb0799d1400cd112d0
Joas Schilling <nickvergessen@gmx.de>
2014-04-02 17:48:04 +0200

    Fix jQuery selector for checkboxes so the settings work again

--- activity/js/settings.js
+++ activity/js/settings.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$('#activity_notifications .personalblock input[type=checkbox]').change(function(){
+	$('#activity_notifications input[type=checkbox]').change(function(){
 		OC.msg.startSaving('#activity_notifications_msg');
 		var post = $( '#activity_notifications' ).serialize();
 		$.post( OC.filePath('activity', 'ajax', 'settings.php'), post, function(data){
