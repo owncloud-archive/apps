2d417aff705bba3af349a56065884ba119faaaff
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-04-04 00:50:15 -0400

    [tx-robot] updated from transifex

--- activity/l10n/pt_BR.php
+++ activity/l10n/pt_BR.php
@@ -5,12 +5,12 @@
 "Loading older activities" => "Carregando atividades antigas.",
 "A file or folder has been <strong>shared</strong>" => "Um arquivo ou pasta foi <strong>compartilhado</strong> ",
 "A new file or folder has been <strong>created</strong> in a shared folder" => "Um novo arquivo ou pasta foi <strong>criado</strong> em uma pasta compartilhada",
-"A file or folder has been <strong>changed</strong> in a shared folder" => "Um arquivo ou pasta foi <strong>criado</strong> em uma pasta compartilhada",
-"A file or folder has been <strong>deleted</strong> from a shared folder" => "Um novo arquivo ou pasta foi <strong>eliminado</strong> de uma pasta compartilhada",
+"A file or folder has been <strong>changed</strong> in a shared folder" => "Um arquivo ou pasta foi <strong>alterado</strong> em uma pasta compartilhada",
+"A file or folder has been <strong>deleted</strong> from a shared folder" => "Um novo arquivo ou pasta foi <strong>apagada</strong> de uma pasta compartilhada",
 "RSS feed" => "Alimentação RSS",
 "No activities yet." => "Nenhuma atividade ainda.",
-"You will see a list of events here when you start to use your %s." => "Você verá aqui uma lista de eventos quando você começar a usar o %s.",
+"You will see a list of events here when you start to use your %s." => "Você verá uma lista de eventos aqui quando começar a usar o seu %s.",
 "Notifications" => "Notificações",
-"Mail" => "Mail",
+"Mail" => "EMail",
 "Stream" => "Fluxo"
 );
