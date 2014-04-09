dc2d758ae7805c53591ec0f8e0584bd8960f3d1b
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-04-10 00:31:33 +0200

    remove unmaintained/moved xmpp app

--- xmpp/ajax/deleteRoster.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$x=new OC_xmpp_login(OCP\Config::getAppValue('xmpp', 'xmppAdminUser',''),OCP\Config::getAppValue('xmpp', 'xmppDefaultDomain',''),OCP\Config::getAppValue('xmpp', 'xmppAdminPasswd',''),OCP\Config::getAppValue('xmpp', 'xmppBOSHURL',''));
-$nx=$x->doLogin(OCP\User::getUser().'@'.OCP\Config::getAppValue('xmpp', 'xmppDefaultDomain',''));
-$nx->deleteRoster($_POST['jid']);
-$x->logout();
-$nx->logout();
-?>
