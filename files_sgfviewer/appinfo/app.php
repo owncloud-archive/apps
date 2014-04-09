973fcb2ce3437372c6caf67c943e907329c90a20
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-04-10 00:25:42 +0200

    remove unmaintained files_sgfviewer app

--- files_sgfviewer/appinfo/app.php
+++
@@ -1,5 +0,0 @@
-<?php
-//load the required files
-OCP\Util::addStyle( 'files_sgfviewer', 'player');
-OCP\Util::addScript( 'files_sgfviewer', 'all.compressed');
-OCP\Util::addScript( 'files_sgfviewer', 'viewer');
