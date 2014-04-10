8406d949d9fa9e1d08af0250a4a5051a50d91a59
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-04-10 00:49:15 -0400

    [tx-robot] updated from transifex

--- activity/l10n/fr.php
+++ activity/l10n/fr.php
@@ -1,7 +1,9 @@
 <?php $TRANSLATIONS = array(
+"Your settings have been updated." => "Vos paramètres ont été mis à jour.",
 "Activity" => "Activité",
 "No more activities to load." => "Plus aucune activité à charger.",
 "Loading older activities" => "Chargement d'activités plus anciennes",
+"A file or folder has been <strong>shared</strong>" => "Un fichier ou un répertoire a été <strong>partagé</strong>",
 "RSS feed" => "Flux RSS",
 "No activities yet." => "Aucune activité.",
 "You will see a list of events here when you start to use your %s." => "Vous verrez ici une liste d'événements lorsque vous commencerez à utiliser votre %s.",
--- admin_migrate/l10n/ast.php
+++ admin_migrate/l10n/ast.php
@@ -1,3 +1,7 @@
 <?php $TRANSLATIONS = array(
+"Export data" => "Esportar datos",
+"All system files and user data" => "Tolos ficheros del sistema y los datos d'usuariu",
+"Only system files" => "Sólo ficheros del sistema",
+"Only user data" => "Sólo datos d'usuariu",
 "Export" => "Esportar"
 );
