daa1f1b547699981a01ee534ed87fb0f86ad16f0
Jenkins for ownCloud <thomas.mueller@tmit.eu>
2014-04-14 00:49:11 -0400

    [tx-robot] updated from transifex

---
+++ admin_dependencies_chk/l10n/km.php
@@ -0,0 +1,7 @@
+<?php $TRANSLATIONS = array(
+"The php-json module is needed by the many applications for inter communications" => "កម្មវិធី​ជា​ច្រើន​ត្រូវ​ការ​ម៉ូឌុល php-json សម្រាប់​អន្តរ​ទំនាក់​ទំនង",
+"The php-gd module is needed to create thumbnails of your images" => "ត្រូវ​ការ​ម៉ូឌុល php-gd ដើម្បី​បង្កើត​រូបតូច​នៃ​រូបភាព​របស់​អ្នក",
+"The php-zip module is needed download multiple files at once" => "ត្រូវ​ការ​ម៉ូឌុល php-zip ដើម្បី​ទាញយក​ឯកសារ​ច្រើង​ក្នុង​ពេល​ព្រម​គ្នា",
+"The php-mb_multibyte module is needed to manage correctly the encoding." => "ត្រូវ​ការ​ម៉ូឌុល php-mb_multibyte ដើម្បី​គ្រប់គ្រង​ការ​អិនកូដ​ឲ្យ​បាន​ត្រឹម​ត្រូវ។",
+"Used by :" => "ប្រើ​ដោយ៖"
+);
--- ownpad_lite/l10n/km.php
+++ ownpad_lite/l10n/km.php
@@ -1,5 +1,6 @@
 <?php $TRANSLATIONS = array(
 "Settings" => "ការកំណត់",
+"Open" => "បើក",
 "Share" => "ចែក​រំលែក",
 "Username" => "ឈ្មោះ​អ្នកប្រើ",
 "Save" => "រក្សាទុក"
