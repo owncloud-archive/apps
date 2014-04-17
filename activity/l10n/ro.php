857fba45f180fb19364ab7183b6b9085a42a99e1
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-04-17 15:04:58 -0400

    [tx-robot] updated from transifex

--- activity/l10n/ro.php
+++ activity/l10n/ro.php
@@ -5,6 +5,7 @@ $TRANSLATIONS = array(
 "Loading older activities" => "Se incarca activitati mai vechi",
 "_%n more..._::_%n more..._" => array("","",""),
 "RSS feed" => "Feed RSS",
-"No activities yet." => "Inca nu exista activitati."
+"No activities yet." => "Inca nu exista activitati.",
+"You will see a list of events here when you start to use your %s." => "Cand incepeti sa folositi propriul %s veti vedea aici o lista a evenimentelor."
 );
 $PLURAL_FORMS = "nplurals=3; plural=(n==1?0:(((n%100>19)||((n%100==0)&&(n!=0)))?2:1));";
