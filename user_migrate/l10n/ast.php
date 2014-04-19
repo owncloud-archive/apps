6cd4fac39a2d425276479bc77eb5520abc449aae
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-04-19 00:50:11 -0400

    [tx-robot] updated from transifex

--- user_migrate/l10n/ast.php
+++ user_migrate/l10n/ast.php
@@ -1,6 +1,9 @@
 <?php
 $TRANSLATIONS = array(
 "Export" => "Esportar",
+"Something went wrong while the export file was being generated" => "Daqué salió mal mentanto se xeneraba'l ficheru d'esportación",
+"An error has occurred" => "Hebo un fallu",
+"Account Export/Import" => "Cuenta Esportar/Importar",
 "Import" => "Importar"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
