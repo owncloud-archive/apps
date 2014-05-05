d272ed78a36fe65912ce8d11494d837ada40f73c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-05 00:49:48 -0400

    [tx-robot] updated from transifex

--- activity/l10n/zh_CN.php
+++ activity/l10n/zh_CN.php
@@ -1,12 +1,19 @@
 <?php
 $TRANSLATIONS = array(
+"Your settings have been updated." => "您的设置已更新。",
 "Activity" => "动态",
 "No more activities to load." => "已无更多动态。",
 "Loading older activities" => "更多动态载入中",
-"_%n more..._::_%n more..._" => array(""),
+"A file or folder has been <strong>shared</strong>" => "一个文件或文件夹已<strong>共享</strong>。",
+"A new file or folder has been <strong>created</strong> in a shared folder" => "一个新文件或文件夹已在一个共享文件夹中<strong>创建</strong>。",
+"A file or folder has been <strong>changed</strong> in a shared folder" => "一个文件或文件夹在共享文件夹中被<strong>修改</strong>。",
+"A file or folder has been <strong>deleted</strong> from a shared folder" => "一个文件或文件夹在共享文件夹中被<strong>删除</strong>。",
+"_%n more..._::_%n more..._" => array("还有 %n 个..."),
 "RSS feed" => "RSS源",
 "No activities yet." => "暂无动态",
 "You will see a list of events here when you start to use your %s." => "当您开始使用%s，您会在此看到一系列事件。",
-"Notifications" => "提示"
+"Notifications" => "提示",
+"Mail" => "邮件",
+"Stream" => "流"
 );
 $PLURAL_FORMS = "nplurals=1; plural=0;";
