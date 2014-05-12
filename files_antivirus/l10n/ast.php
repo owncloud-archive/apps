9970bdfa41155bcfb034b31f5c78b3b8d86f9118
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-12 00:49:46 -0400

    [tx-robot] updated from transifex

--- files_antivirus/l10n/ast.php
+++ files_antivirus/l10n/ast.php
@@ -1,12 +1,28 @@
 <?php
 $TRANSLATIONS = array(
+"Greetings {user}," => "Hola {user},",
+"Sorry, but a malware was detected in a file you tried to upload and it had to be deleted." => "Sentímoslo, pero deteutóse un malware nun ficheru que quies xubir y tuvo que desaniciase.",
+"This email is a notification from {host}. Please, do not reply." => "Esti corréu-e ye una notificación de {host}. Por favor, nun contestes al mensaxe.",
+"File uploaded: {file}" => "Ficheru xubíu: {file}",
+"Antivirus Configuration" => "Axustes d'Antivirus",
 "Mode" => "Mou",
 "Executable" => "Executable",
 "Daemon" => "Degorriu",
+"Daemon (Socket)" => "Degorriu (Socket)",
+"Socket" => "Socket",
+"Clamav Socket." => "Clamav Socket.",
 "Not required in Executable Mode." => "Nun se requier nel mou executable.",
 "Host" => "Equipu",
+"Address of Antivirus Host." => "Direición del Host del Antivirus. ",
 "Port" => "Puertu",
+"Port number of Antivirus Host." => "Númberu de puertu del Host del Antivirus",
+"Stream Length" => "Llonxitú de la tresmisión",
+"ClamAV StreamMaxLength value in bytes." => "Valor StreamMaxLength del ClamAV en bytes.",
+"Path to clamscan" => "Camín al clamscan",
+"Path to clamscan executable." => "Camín al executable clamscan.",
 "Not required in Daemon Mode." => "Nun se requier nel mou degorriu.",
+"Action for infected files found while scanning" => "Aición pa los ficheros infectaos que s'atopen mientres l'escanéu",
+"Only log" => "Namái rexistrar",
 "Delete file" => "Desanciar ficheru",
 "Save" => "Guardar"
 );
--- firstrunwizard/l10n/ast.php
+++ firstrunwizard/l10n/ast.php
@@ -1,12 +1,14 @@
 <?php
 $TRANSLATIONS = array(
 "Welcome to %s" => "Bienllegáu a %s",
+"Your personal web services. All your files, contacts, calendar and more, in one place." => "La to web de servicios personal. Tolos tos ficheros, contautos, calendariu y más, nun llugar.",
 "Get the apps to sync your files" => "Obtener les aplicaciones pa sincronizar ficheros",
 "Connect your desktop apps to %s" => "Coneuta les tos aplicaciones d'escritoriu a %s",
 "Connect your Calendar" => "Coneuta'l to Calendariu",
 "Connect your Contacts" => "Coneuta los tos Contautos",
 "Access files via WebDAV" => "Ficheru d'accesu vía WebDAV",
 "Documentation" => "Documentación",
-"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Hai más información na <a target=\"_blank\" href=\"%s\">documentación y nel nuesu <a target=\"_blank\" href=\"http://owncloud.org\">sitiu web</a>."
+"There’s more information in the <a target=\"_blank\" href=\"%s\">documentation</a> and on our <a target=\"_blank\" href=\"http://owncloud.org\">website</a>." => "Hai más información na <a target=\"_blank\" href=\"%s\">documentación y nel nuesu <a target=\"_blank\" href=\"http://owncloud.org\">sitiu web</a>.",
+"If you like ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">recommend it to your friends</a>!" => "Si te presta ownCloud, <a href=\"mailto:?subject=ownCloud&body=ownCloud is a great open software to sync and share your files. You can freely get it from http://owncloud.org\">recomiénda-yoslu a los tos collacios</a>!"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
