f746da6965bd1b8240209166cd7d70cfee86e236
Jan-Christoph Borchardt <hey@jancborchardt.net>
2014-05-14 11:40:18 +0200

    change colored change icon to black/grey for less distraction

--- activity/lib/data.php
+++ activity/lib/data.php
@@ -351,7 +351,7 @@ class Data
 		switch ($type)
 		{
 			case self::TYPE_SHARE_CHANGED:
-				return 'icon-change-color';
+				return 'icon-change';
 			case self::TYPE_SHARE_CREATED:
 				return 'icon-add-color';
 			case self::TYPE_SHARE_DELETED:
