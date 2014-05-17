280c8aad39033075dba3c47b660fa5a2b5005310
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-17 00:48:59 -0400

    [tx-robot] updated from transifex

--- activity/l10n/ur_PK.php
+++ activity/l10n/ur_PK.php
@@ -1,5 +1,8 @@
 <?php
 $TRANSLATIONS = array(
-"_%n more..._::_%n more..._" => array("","")
+"Activity" => "سرگرمی",
+"_%n more..._::_%n more..._" => array("",""),
+"RSS feed" => "ارایس ایس فیڈ",
+"No activities yet." => "ابھی کوئ سرگرمیاں نہیں"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
---
+++ tasks/l10n/ur_PK.php
@@ -0,0 +1,5 @@
+<?php
+$TRANSLATIONS = array(
+"Delete" => "حذف کریں"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
