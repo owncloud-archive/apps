835e0eb9b675333d05a35624ef899a9f9f5503d3
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-05-18 00:48:44 -0400

    [tx-robot] updated from transifex

--- external/l10n/ur_PK.php
+++ external/l10n/ur_PK.php
@@ -1,5 +1,9 @@
 <?php
 $TRANSLATIONS = array(
+"External Sites" => "بیرونی سائٹس",
+"Name" => "اسم",
+"URL" => "یو ار ایل",
+"Remove site" => "سائٹ ہٹایں",
 "Add" => "شامل کریں"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
