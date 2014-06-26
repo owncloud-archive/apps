66a9f1e2706f2176866ae31cb6ad7d9da9c3a9b2
Vincent Petry <pvince81@owncloud.com>
2014-06-26 18:50:43 +0200

    Setup file locking storage wrapped in setup hook
    This fixes an issue when using file locking + encryption and try to list
    a shared mount in the web UI.
    
    Using the setup hook prevents having setupFS() be called twice in a row
    and mess up with the mount points.

--- files_locking/lib/lockingwrapper.php
+++ files_locking/lib/lockingwrapper.php
@@ -178,5 +178,21 @@ class LockingWrapper extends Wrapper {
 		return $result;
 	}
 
+	/**
+	 * Setup the storate wrapper callback
+	 */
+	public static function setupWrapper() {
+		// Set up flock
+		\OC\Files\Filesystem::addStorageWrapper('oc_flock', function ($mountPoint, $storage) {
+			/**
+			 * @var \OC\Files\Storage\Storage $storage
+			 */
+			if ($storage instanceof \OC\Files\Storage\Storage && $storage->isLocal()) {
+				return new \OCA\Files_Locking\LockingWrapper(array('storage' => $storage));
+			} else {
+				return $storage;
+			}
+		});
+	}
 
 }
