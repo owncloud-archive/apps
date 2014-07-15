92051e75a3ab4cf1a59ea60a1a8aa462529e3b02
Thomas Müller <thomas.mueller@tmit.eu>
2014-07-15 12:00:07 +0200

    - adding OCP\JSON::checkAppEnabled('external') where needed - in case a requested site is unknown we redirect to the default page - fix X-Frame-Option header (addHeader method is so wrong) - adding notes on iframe and http/https - getting rid of usage of classpath and rename class OC_External to \OCA\External\External - re-enabling save message - fix javascript coding style - show start saving message - validate url to start with http:// or https://

--- external/lib/external.php
+++ external/lib/external.php
@@ -21,10 +21,12 @@
  *
  */
 
-class OC_External {
+namespace OCA\External;
+
+class External {
 
 	public static function getSites() {
-		if (($sites = json_decode(OCP\Config::getAppValue("external", "sites", ''))) != null) {
+		if (($sites = json_decode(\OCP\Config::getAppValue("external", "sites", ''))) != null) {
 			return $sites;
 		}
 
