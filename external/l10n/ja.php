32877d3b5909261bc82b72ebd11490ed0c3cf154
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-22 00:48:21 -0400

    [tx-robot] updated from transifex

--- external/l10n/ja.php
+++ external/l10n/ja.php
@@ -1,6 +1,11 @@
 <?php
 $TRANSLATIONS = array(
+"Please enter valid urls - they have to start with either http:// or https://" => "正しいURLを入力してください。http:// または、https:// で始まる必要があります。",
+"External sites saved." => "外部サイトを保存しました。",
 "External Sites" => "外部サイト",
+"Please note that some browsers will block displaying of sites via http if you are running https." => "ブラウザーによっては、https で動作している時に http で接続するとブロックされてしまうことがあるのでご注意ください。",
+"Furthermore please note that many sites these days disallow iframing due to security reasons." => "さらに、今日セキュリティ上の理由から多くのサイトで iframe の使用が禁止されているのでご注意ください。",
+"We highly recommend to test the configured sites below properly." => "以下の設定でサイトが設定されているか、テストすることを強くお勧めします。",
 "Name" => "名前",
 "URL" => "URL",
 "Remove site" => "サイトを削除",
--- external/l10n/nl.php
+++ external/l10n/nl.php
@@ -1,6 +1,11 @@
 <?php
 $TRANSLATIONS = array(
+"Please enter valid urls - they have to start with either http:// or https://" => "Geef geldige urls op - die moeten starten met http:// of https://",
+"External sites saved." => "Externe sites bewaard.",
 "External Sites" => "Externe sites",
+"Please note that some browsers will block displaying of sites via http if you are running https." => "Let op dat sommige browsers http sites niet tonen als u https gebruikt.",
+"Furthermore please note that many sites these days disallow iframing due to security reasons." => "Let er ook op dat veel sites het gebruik van iframes wegens beveiligingsredenen tegengaan.",
+"We highly recommend to test the configured sites below properly." => "We adviseren om de hieronder geconfigureerde sites goed te testen.",
 "Name" => "Naam",
 "URL" => "URL",
 "Remove site" => "Verwijder site",
