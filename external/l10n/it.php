451a5880ca86fdf0aa9d3f7d2009d880cc47f0b5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-23 00:48:24 -0400

    [tx-robot] updated from transifex

--- external/l10n/it.php
+++ external/l10n/it.php
@@ -5,6 +5,7 @@ $TRANSLATIONS = array(
 "External Sites" => "Siti esterni",
 "Please note that some browsers will block displaying of sites via http if you are running https." => "Nota che alcuni browser bloccheranno la visualizzazione dei siti tramite http se stai utilizzando https.",
 "Furthermore please note that many sites these days disallow iframing due to security reasons." => "Nota inoltre che molti siti attualmente non consentono l'utilizzo di iframe per motivi di sicurezza.",
+"We highly recommend to test the configured sites below properly." => "Suggeriamo vivamente di provare i seguenti siti configurati.",
 "Name" => "Nome",
 "URL" => "URL",
 "Remove site" => "Rimuovi il sito",
