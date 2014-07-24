f04b30c0cc9d56078772ea737fbd5f6bb4c1b366
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-24 00:48:31 -0400

    [tx-robot] updated from transifex

--- external/l10n/pt_PT.php
+++ external/l10n/pt_PT.php
@@ -1,6 +1,11 @@
 <?php
 $TRANSLATIONS = array(
+"Please enter valid urls - they have to start with either http:// or https://" => "Por favor introduza endereços válidos - têm de começar por http:// ou https://",
+"External sites saved." => "Páginas externas guardadas.",
 "External Sites" => "Páginas Externas",
+"Please note that some browsers will block displaying of sites via http if you are running https." => "Por favor note que alguns navegadores vão bloquear a mostragem das paginas via http se está a correr https.",
+"Furthermore please note that many sites these days disallow iframing due to security reasons." => "Além disso por favor note que muitas paginas nestes dias desautorizam iframing devido a razões de segurança.",
+"We highly recommend to test the configured sites below properly." => "Nós recomendamos a testar as configurações das paginas abaixo correctamente.",
 "Name" => "Nome",
 "URL" => "URL",
 "Remove site" => "Remover página",
