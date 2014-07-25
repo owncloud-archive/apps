8b388625e0980e2291984733645ad0eadffdb140
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-07-25 00:48:23 -0400

    [tx-robot] updated from transifex

--- external/l10n/eu.php
+++ external/l10n/eu.php
@@ -1,6 +1,11 @@
 <?php
 $TRANSLATIONS = array(
-"External Sites" => "Kanpoko lekuak",
+"Please enter valid urls - they have to start with either http:// or https://" => "Sartu baliozko helbideak - hasi behar dute http:// edo https://",
+"External sites saved." => "Kanpoko guneak  gordeta.",
+"External Sites" => "Kanpoko guneak",
+"Please note that some browsers will block displaying of sites via http if you are running https." => "Egin kontu zenbait nabigatzailek blokea dezaketela guneen http bidezko bistaratzea https erabiltzen ari bazara.",
+"Furthermore please note that many sites these days disallow iframing due to security reasons." => "Gainera kontuan izan egun hauetan zenbait gunek ez dutela iframing onartzen seguritatea arrazoiengatik.",
+"We highly recommend to test the configured sites below properly." => "Guztiz gomendatzen dugu behar bezala probatzea konfiguratutako guneak.",
 "Name" => "Izena",
 "URL" => "URL",
 "Remove site" => "Ezabatu lekua",
