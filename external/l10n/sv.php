63fb88bc8a0f5da1e8bad8ba9dcb8c647fc9afbc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-24 00:49:04 -0400

    [tx-robot] updated from transifex

--- external/l10n/sv.php
+++ external/l10n/sv.php
@@ -1,9 +1,12 @@
 <?php
 $TRANSLATIONS = array(
+"Please enter valid urls - they have to start with either http://, https:// or /" => "Vänligen ange giltiga url:er - de måste starta med antingen http://, https:// eller /",
 "External sites saved." => "Extern sida sparad.",
 "External Sites" => "Externa webbplatser",
+"We highly recommend to test the configured sites below properly." => "Vi rekommenderar starkt att testa de konfigurerade sidorna här nedan ordentligt.",
 "Name" => "Namn",
 "URL" => "URL",
+"Select an icon" => "Välj en ikon",
 "Remove site" => "Ta bort webbplats",
 "Add" => "Lägg till"
 );
