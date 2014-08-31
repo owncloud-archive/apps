4e876b3595c70e7965906ed87ff6fba50b28265b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-08-31 13:03:22 -0400

    [tx-robot] updated from transifex

--- ownpad_lite/l10n/az.php
+++ ownpad_lite/l10n/az.php
@@ -4,7 +4,11 @@ $TRANSLATIONS = array(
 "User %s shared quick document %s with you." => "İstifadəçi %s təcili sənədi yayımladı %s sizinlə.",
 "My pad" => "Mənim qeydiyyatım",
 "Settings" => "Quraşdırmalar",
+"Pad Title" => "Pad başlıq",
+"Open" => "Aç",
+"Share with" => "Bununla yaımla",
 "Share" => "Yayımla",
+"Etherpad service URL" => "Etherpad URL xidməti",
 "Username" => "İstifadəçi adı",
 "Save" => "Saxlamaq"
 );
