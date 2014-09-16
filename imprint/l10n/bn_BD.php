f395a5a6cd4c448adf55db6568922e3392b29a1e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-16 00:54:06 -0400

    [tx-robot] updated from transifex

--- imprint/l10n/bn_BD.php
+++ imprint/l10n/bn_BD.php
@@ -5,6 +5,9 @@ $TRANSLATIONS = array(
 "The content of the legal notice has to be configured first" => "প্রথমে আইনী বিজ্ঞপ্তির বিষয়বস্তু কনফিগার করতে হবে",
 "That configuration is done in the administration section." => "ঐ কনফিগারেসনটি প্রশাসন শাখা থেকে করা হয়।",
 "Imprint" => "প্রতিচ্ছাপ",
-"Placement" => "স্থাপন"
+"Placement" => "স্থাপন",
+"Standalone app" => "অসাধারণ অ্যাপ",
+"During login" => "লগইনকালে",
+"Content" => "বিষয়বস্তু"
 );
 $PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
