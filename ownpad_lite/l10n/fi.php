0dc0c890c1f3693db07498debef6569dda6f022b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-09-25 00:48:40 -0400

    [tx-robot] updated from transifex

---
+++ ownpad_lite/l10n/fi.php
@@ -0,0 +1,11 @@
+<?php
+$TRANSLATIONS = array(
+"My pad" => "minun pad",
+"Settings" => "asetukset",
+"Pad Title" => "pad otsikko",
+"Open" => "avata",
+"Etherpad service URL" => "etherpad palvelun url",
+"Username" => "Käyttäjätunnus",
+"Save" => "tallentaa"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n != 1);";
