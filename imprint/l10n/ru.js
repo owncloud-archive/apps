67524111ddbad99d96fb70e7d6cfadd139cba97c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2014-11-22 00:51:05 -0500

    [tx-robot] updated from transifex

--- imprint/l10n/ru.js
+++ imprint/l10n/ru.js
@@ -7,6 +7,7 @@ OC.L10N.register(
     "That configuration is done in the administration section." : "Эта конфигурация делается в разделе администрирования.",
     "That configuration has to be done by the system administration." : "Эту конфигурацию должен сделать системный администратор",
     "Imprint" : "Штамп",
+    "Reference" : "Ссылка",
     "Header left" : "Заголовок слева",
     "Header right" : "Заголовок справа",
     "As guest" : "Как гость",
@@ -14,6 +15,7 @@ OC.L10N.register(
     "Footer right" : "Низ страницы справа",
     "At login" : "При логине",
     "Application" : "Приложение",
-    "Content" : "Содержание"
+    "Content" : "Содержание",
+    "You can use plain text, markdown notation or html markup with inline style attributes. " : "Вы можете использовать простой текст, нотацию markdown или разметку HTML со встроенными стилями в атрибутах тегов."
 },
 "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);");
--- imprint/l10n/sk_SK.js
+++ imprint/l10n/sk_SK.js
@@ -15,6 +15,7 @@ OC.L10N.register(
     "Footer right" : "Päta vpravo",
     "At login" : "Pri prihlasovaní",
     "Application" : "Aplikácia",
-    "Content" : "Obsah"
+    "Content" : "Obsah",
+    "You can use plain text, markdown notation or html markup with inline style attributes. " : "Môžete použiť obyčajný text, formátovaný zápis alebo html formátovanie. "
 },
 "nplurals=3; plural=(n==1) ? 0 : (n>=2 && n<=4) ? 1 : 2;");
