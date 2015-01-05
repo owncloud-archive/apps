7915e813da08c204e1153e82043c5511359b880f
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-05 00:50:44 -0500

    [tx-robot] updated from transifex

--- imprint/l10n/nb_NO.js
+++ imprint/l10n/nb_NO.js
@@ -6,8 +6,15 @@ OC.L10N.register(
     "The content of the legal notice has to be configured first" : "Innholdet i de juridiske merknadene må konfigureres først",
     "That configuration has to be done by the system administration." : "Den konfigureringen må gjøres av systemadministrasjonen.",
     "Imprint" : "Avtrykk",
+    "Reference" : "Referanse",
     "Header left" : "Overskrift venstre",
     "Header right" : "Overskrift høyre",
-    "Content" : "Innhold"
+    "As guest" : "Som gjest",
+    "Footer left" : "Bunntekst venstre",
+    "Footer right" : "Bunntekst høyre",
+    "At login" : "Ved innlogging",
+    "Application" : "Applikasjon",
+    "Content" : "Innhold",
+    "You can use plain text, markdown notation or html markup with inline style attributes. " : "Du kan bruke ren tekst, markdown-notasjon eller html-format inkludert stil-attributter."
 },
 "nplurals=2; plural=(n != 1);");
