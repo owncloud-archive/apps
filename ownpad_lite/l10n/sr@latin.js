8820dcb017c0d2759b297179b29824303449464a
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-07 00:50:51 -0500

    [tx-robot] updated from transifex

--- ownpad_lite/l10n/sr@latin.js
+++ ownpad_lite/l10n/sr@latin.js
@@ -1,9 +1,15 @@
 OC.L10N.register(
     "ownpad_lite",
     {
+    "Document was shared" : "Dokument je deljen",
+    "User %s shared quick document %s with you." : "Korisnik %s je podelio brzi dokument %s sa Vama.",
+    "My pad" : "Moje blokče",
     "Settings" : "Podešavanja",
+    "Pad Title" : "Naslov Blokčeta",
+    "Open" : "Otvori",
     "Share with" : "Podeli sa",
     "Share" : "Podeli",
+    "Etherpad service URL" : "Etherpad servisni URL",
     "Username" : "Korisničko ime",
     "Save" : "Snimi"
 },
