10d100f6c7ea525f1db7ae45e3f60dc702a1b664
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-28 00:51:11 -0500

    [tx-robot] updated from transifex

--- user_migrate/l10n/mn.js
+++ user_migrate/l10n/mn.js
@@ -3,6 +3,7 @@ OC.L10N.register(
     {
     "Export" : "Экспорт",
     "Something went wrong while the export file was being generated" : "Файлыг экспорт хийх үйл явцад ямар нэгэн алдаа гарлаа",
+    "An error has occurred" : "Алдаа гарсан байна",
     "Account Export/Import" : "Аккаунтыг Експорт болон Импорт хийх",
     "Import" : "Импорт"
 },
