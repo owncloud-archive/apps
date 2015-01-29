b0cbce67147ac6add7718168c388730a9d186384
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-01-29 00:50:53 -0500

    [tx-robot] updated from transifex

--- user_migrate/l10n/de.js
+++ user_migrate/l10n/de.js
@@ -4,7 +4,7 @@ OC.L10N.register(
     "Export" : "Export",
     "Something went wrong while the export file was being generated" : "Beim Export der Datei ist etwas schiefgegangen.",
     "An error has occurred" : "Es ist ein Fehler aufgetreten.",
-    "Account Export/Import" : "Konto Export/Import",
+    "Account Export/Import" : "Export/Import des Kontos",
     "Import" : "Importieren"
 },
 "nplurals=2; plural=(n != 1);");
