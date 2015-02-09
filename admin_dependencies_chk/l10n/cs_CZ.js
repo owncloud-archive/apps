5ab8afdda62b77ce207d9d8b4ec6e13102596d9e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-09 00:50:49 -0500

    [tx-robot] updated from transifex

--- admin_dependencies_chk/l10n/cs_CZ.js
+++ admin_dependencies_chk/l10n/cs_CZ.js
@@ -4,7 +4,7 @@ OC.L10N.register(
     "The php-json module is needed by the many applications for inter communications" : "Modul php-json je potřeba pro vzájemnou komunikaci mnoha aplikací",
     "The php-curl module is needed to fetch the page title when adding a bookmark" : "Modul php-curl je potřeba pro získání názvu stránky při vytváření záložky",
     "The php-gd module is needed to create thumbnails of your images" : "Modul php-gd je potřeba pro tvorbu náhledů Vašich obrázků",
-    "The php-ldap module is needed connect to your ldap server" : "Modul php-ldap je potřeba pro připojení k Vašemu ldap serveru",
+    "The php-ldap module is needed connect to your ldap server" : "Modul php-ldap je potřeba pro připojení k ldap serveru",
     "The php-bcmath module is needed to support AD primary groups" : "Pro podporu AD primárních skupin je třeba modul php-bcmath",
     "The php-zip module is needed to download multiple files at once" : "Pro stahování více souborů najednou je třeba modul php-zip",
     "The php-mb_multibyte module is needed to manage correctly the encoding." : "Modul php-mb_multibyte je potřeba pro správnou funkci kódování.",
