61d3aaf89e93ec8d69cfef9ea2b53ed161e0480b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-04-18 00:50:35 -0400

    [tx-robot] updated from transifex

--- external/l10n/ro.js
+++ external/l10n/ro.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Please enter valid urls - they have to start with either http://, https:// or /" : "Va rugam introduceti adrese valide - trebuie sa inceapa cu  http://, https:// sau /",
     "External sites saved." : "Pagini externe salvate .",
     "External Sites" : "Pagini externe",
+    "Please note that some browsers will block displaying of sites via http if you are running https." : "Te rog noteaza ca unele browsere vor bloca afișarea siteurilor prin http daca rulezi https",
     "We highly recommend to test the configured sites below properly." : "Recomandam insistent testarea conforma a site-urilor configurate mai jos ",
     "Name" : "Nume",
     "URL" : "URL",
