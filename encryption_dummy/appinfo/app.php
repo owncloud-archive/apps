c6b7ca98580f5905da6bf42528afd3c92b23c65a
Joas Schilling <nickvergessen@owncloud.com>
2015-04-30 14:52:38 +0200

    Fix minor issues

--- encryption_dummy/appinfo/app.php
+++ encryption_dummy/appinfo/app.php
@@ -1,8 +1,8 @@
 <?php
 
 $manager = \OC::$server->getEncryptionManager();
-$module = new \OCA\Encryption_Dummy\DummyModule();
-$manager->registerEncryptionModule('OC_DUMMY_MODULE', 'Dummy Encryption Module', function() use ($module) {
+$manager->registerEncryptionModule('OC_DUMMY_MODULE', 'Dummy Encryption Module', function() {
+	$module = new \OCA\Encryption_Dummy\DummyModule();
 	return $module;
 });
 
