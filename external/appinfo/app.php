658b603c3d8790146f9ca4b1699a1e6cf3bfb642
Joas Schilling <nickvergessen@owncloud.com>
2015-05-13 08:50:55 +0200

    Do not create the navigation if the app is not enabled anymore

--- external/appinfo/app.php
+++ external/appinfo/app.php
@@ -26,14 +26,19 @@ use OCA\External\External;
 OCP\App::registerAdmin('external', 'settings');
 
 $sites = External::getSites();
-for ($i = 0; $i < sizeof($sites); $i++) {
-	OCP\App::addNavigationEntry(
-		array(
-			'id'    => 'external_index' . ($i + 1),
-			'order' => 80 + $i,
-			'href'  => OCP\Util::linkToRoute('external_index', array('id'=> $i + 1)),
-			'icon'  => OCP\Util::imagePath('external', !empty($sites[$i][2]) ? $sites[$i][2] : 'external.svg'),
-			'name'  => $sites[$i][0]
-		)
-	);
+if (!empty($sites)) {
+	$urlGenerator = \OC::$server->getURLGenerator();
+	$navigationManager = \OC::$server->getNavigationManager();
+	for ($i = 0; $i < sizeof($sites); $i++) {
+		$navigationEntry = function () use ($i, $urlGenerator, $sites) {
+			return [
+				'id'    => 'external_index' . ($i + 1),
+				'order' => 80 + $i,
+				'href' => $urlGenerator->linkToRoute('external_index', ['id'=> $i + 1]),
+				'icon' => $urlGenerator->imagePath('external', !empty($sites[$i][2]) ? $sites[$i][2] : 'external.svg'),
+				'name' => $sites[$i][0],
+			];
+		};
+		$navigationManager->add($navigationEntry);
+	}
 }
