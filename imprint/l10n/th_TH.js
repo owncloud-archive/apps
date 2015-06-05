80e370019027d1f81827cc2e90877c0de8dce7e5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-05 00:50:55 -0400

    [tx-robot] updated from transifex

--- imprint/l10n/th_TH.js
+++ imprint/l10n/th_TH.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "imprint",
     {
+    "Imprint" : "ประทับ",
     "Content" : "เนื้อหา"
 },
 "nplurals=1; plural=0;");
--- user_migrate/l10n/th_TH.js
+++ user_migrate/l10n/th_TH.js
@@ -2,8 +2,9 @@ OC.L10N.register(
     "user_migrate",
     {
     "Export" : "ส่งออก",
-    "Something went wrong while the export file was being generated" : "เกิดข้อผิดพลาดบางประการในระหว่างการส่งออกไฟล์",
-    "An error has occurred" : "เกิดข้อผิดพลาดบางประการ",
+    "Something went wrong while the export file was being generated" : "เกิดข้อผิดพลาดบางอย่างระหว่างการส่งออกไฟล์",
+    "An error has occurred" : "เกิดข้อผิดพลาดบางอย่าง",
+    "Account Export/Import" : "บัญชี นำเข้า/ส่งออก",
     "Import" : "นำเข้า"
 },
 "nplurals=1; plural=0;");
