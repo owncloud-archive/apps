d7093514342b1a3eadea81e54c25b8ad0117009e
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-06-06 00:50:57 -0400

    [tx-robot] updated from transifex

--- ownpad_lite/l10n/th_TH.js
+++ ownpad_lite/l10n/th_TH.js
@@ -1,6 +1,8 @@
 OC.L10N.register(
     "ownpad_lite",
     {
+    "Document was shared" : "เอกสารถูกแชร์",
+    "User %s shared quick document %s with you." : "ผู้ใช้ %s ได้แชร์เอกสาร %s อย่างรวดเร็วกับคุณ",
     "My pad" : "แพ็ดของฉัน",
     "Settings" : "ตั้งค่า",
     "Pad Title" : "ชื่อของ Pad",
