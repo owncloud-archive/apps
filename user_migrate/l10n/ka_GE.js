c1c6873a7e5315c3b833234babdf0ad880ff15ec
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-04 00:51:35 -0400

    [tx-robot] updated from transifex

--- user_migrate/l10n/ka_GE.js
+++ user_migrate/l10n/ka_GE.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Export" : "ექსპორტი",
     "Something went wrong while the export file was being generated" : "მოხდა შეცდომა ექსპორტ ფაილის გენერაციის დროს",
     "An error has occurred" : "დაფიქსირდა შეცდომა",
+    "Account Export/Import" : "მომხმარებელთ ანგარიშების იმპორტი/ექსპორტი",
     "Import" : "იმპორტი"
 },
 "nplurals=1; plural=0;");
