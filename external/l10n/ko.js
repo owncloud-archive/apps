7db019490caa86ba34a03d4b1712d88af401b3b2
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-10-18 00:52:10 -0400

    [tx-robot] updated from transifex

--- external/l10n/ko.js
+++ external/l10n/ko.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "external",
     {
     "Please enter valid urls - they have to start with either http://, https:// or /" : "올바른 URL을 입력하십시오. http://, http://, /로 시작하여야 합니다",
-    "External sites saved." : "외부 사이트를 저장하였습니다.",
+    "External sites saved." : "외부 사이트를 저장했습니다.",
     "External Sites" : "외부 사이트",
     "Please note that some browsers will block displaying of sites via http if you are running https." : "일부 웹 브라우저에서는 HTTPS로 접속한 사이트 내에서 HTTP 사이트를 표시하는 것을 거부할 수도 있습니다.",
     "Furthermore please note that many sites these days disallow iframing due to security reasons." : "또한 많은 웹 사이트에서 보안상의 이유로 iframe 사용을 금지하고 있습니다.",
--- ownpad_lite/l10n/ko.js
+++ ownpad_lite/l10n/ko.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "ownpad_lite",
     {
     "Document was shared" : "문서 공유됨",
-    "User %s shared quick document %s with you." : "%s 님이 빠른 문서 %s을(를) 공유하였습니다.",
+    "User %s shared quick document %s with you." : "%s 님이 빠른 문서 %s을(를) 공유했습니다.",
     "My pad" : "내 패드",
     "Settings" : "설정",
     "Pad Title" : "패드 제목",
