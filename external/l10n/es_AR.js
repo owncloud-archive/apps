d9e9270965f57134d9c45fb3d3e4b7ef23e20301
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-11-24 00:51:30 -0500

    [tx-robot] updated from transifex

--- external/l10n/es_AR.js
+++ external/l10n/es_AR.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "external",
     {
+    "Please enter valid urls - they have to start with either http://, https:// or /" : "Ingrese URL válidos: tienen que comenzar con http://, https:// o /",
     "External Sites" : "Sitios externos",
     "Name" : "Nombre",
     "URL" : "URL",
