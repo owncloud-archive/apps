32cc65d36ce8111965ee9107b8e211d90f0c2246
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-04-13 00:51:16 -0400

    [tx-robot] updated from transifex

--- external/l10n/de_AT.js
+++ external/l10n/de_AT.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "external",
     {
+    "Name" : "Name",
     "Add" : "Hinzufügen"
 },
 "nplurals=2; plural=(n != 1);");
--- impress/l10n/de_AT.js
+++ impress/l10n/de_AT.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "impress",
     {
+    "Name" : "Name",
     "Date" : "Datum"
 },
 "nplurals=2; plural=(n != 1);");
