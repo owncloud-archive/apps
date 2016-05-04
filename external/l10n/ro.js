657fa71429762616046212a658e2b874bd100365
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-05-04 00:51:22 -0400

    [tx-robot] updated from transifex

--- external/l10n/ro.js
+++ external/l10n/ro.js
@@ -1,11 +1,12 @@
 OC.L10N.register(
     "external",
     {
-    "Please enter valid urls - they have to start with either http://, https:// or /" : "Va rugam introduceti adrese valide - trebuie sa inceapa cu  http://, https:// sau /",
-    "External sites saved." : "Pagini externe salvate .",
+    "Please enter valid urls - they have to start with either http://, https:// or /" : "Vă rugăm să introduceți adrese valide - trebuie să înceapă cu  http://, https:// sau /",
+    "External sites saved." : "Pagini externe salvate.",
     "External Sites" : "Pagini externe",
-    "Please note that some browsers will block displaying of sites via http if you are running https." : "Te rog noteaza ca unele browsere vor bloca afișarea siteurilor prin http daca rulezi https",
-    "We highly recommend to test the configured sites below properly." : "Recomandam insistent testarea conforma a site-urilor configurate mai jos ",
+    "Please note that some browsers will block displaying of sites via http if you are running https." : "Rețineți că unele navigatoare Web vor bloca afișarea paginilor livrate prin trafic necriptat (HTTP) dacă accesați pagini prin protocolul criptat HTTPS.",
+    "Furthermore please note that many sites these days disallow iframing due to security reasons." : "În plus, vă rugăm să rețineți că în prezent multe site-uri Web interzic folosirea tehnicilor iframe din motive de securitate.",
+    "We highly recommend to test the configured sites below properly." : "Recomandăm insistent testarea îndeaproape a site-urilor configurate mai jos. ",
     "Name" : "Nume",
     "URL" : "URL",
     "Select an icon" : "Selectați o iconiță",
