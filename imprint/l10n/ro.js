c3399cba3d8c56dca1f85202fc259414652c2142
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-05-21 00:51:23 -0400

    [tx-robot] updated from transifex

--- imprint/l10n/ro.js
+++ imprint/l10n/ro.js
@@ -1,13 +1,20 @@
 OC.L10N.register(
     "imprint",
     {
-    "Legal notice" : "Notite legale",
+    "Legal notice" : "Înștiințare legală",
     "Nothing here yet" : "Nimic aici. Încarcă ceva!",
+    "The content of the legal notice has to be configured first" : "Conținutul înștiințării legale trebuie mai întâi configurat",
+    "That configuration has to be done by the system administration." : "Această configurare trebuie făcută de un administrator de sistem.",
     "Imprint" : "Imprint",
-    "Reference" : "Referințe",
+    "Reference" : "Referință",
+    "Header left" : "Antet stânga",
+    "Header right" : "Antet dreapta",
     "As guest" : "Ca vizitator",
+    "Footer left" : "Subsol stânga",
+    "Footer right" : "Subsol dreapta",
     "At login" : "La autentificare",
     "Application" : "Aplicație",
-    "Content" : "Continut"
+    "Content" : "Conținut",
+    "You can use plain text, markdown notation or html markup with inline style attributes. " : "Poți folosi text simplu, notație markdown sau format html cu atribute de stil incluse."
 },
 "nplurals=3; plural=(n==1?0:(((n%100>19)||((n%100==0)&&(n!=0)))?2:1));");
