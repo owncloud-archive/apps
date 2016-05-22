de87fff60bef2df4920d01e08d0868b9dc33544d
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-05-22 00:51:16 -0400

    [tx-robot] updated from transifex

--- impress/l10n/cs_CZ.js
+++ impress/l10n/cs_CZ.js
@@ -2,7 +2,7 @@ OC.L10N.register(
     "impress",
     {
     "Documentation" : "Dokumentace",
-    "No Impress files are found in your ownCloud. Please upload a .impress file." : "Nebyly nalezeny žádné soubory impress ve Vašem ownCloud. Nahrajte, prosím, soubor impress.",
+    "No Impress files are found in your ownCloud. Please upload a .impress file." : "Nebyly nalezeny žádné soubory impress ve Vašem ownCloud. Nahrajte prosím soubor impress.",
     "Name" : "Název",
     "Size" : "Velikost",
     "Date" : "Datum"
