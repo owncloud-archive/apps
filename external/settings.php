28efcd604f220b5589032a64ece290ad9a2abc22
Stephen Colebrook <scolebrook@mac.com>
2016-07-01 10:22:01 -0400

    Removed private api call.

--- external/settings.php
+++ external/settings.php
@@ -8,7 +8,7 @@ OCP\Util::addscript( "external", "admin" );
 $tmpl = new OCP\Template( 'external', 'settings');
 
 $images = glob(\OC_App::getAppPath('external') . '/img/*.*');
-$theme = \OC_Util::getTheme();
+$theme = \OC::$server->getSystemConfig()->getValue('theme', '');
 if (file_exists(\OC::$SERVERROOT . "/themes/$theme/apps/external/img/")) {
 	$images = array_merge($images, glob(\OC::$SERVERROOT . "/themes/$theme/apps/external/img/*.*"));
 }
