91e73b8378fa6892341d0e968fd9e38ed59e161c
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-13 00:50:57 -0400

    [tx-robot] updated from transifex

--- imprint/l10n/ast.js
+++ imprint/l10n/ast.js
@@ -6,8 +6,15 @@ OC.L10N.register(
     "The content of the legal notice has to be configured first" : "El conteníu de la noticia llegal tien de configurase primero",
     "That configuration has to be done by the system administration." : "Esa configuración tien de facese pela alministración del sistema.",
     "Imprint" : "Imprint",
+    "Reference" : "Referencia",
     "Header left" : "Testera esquierda",
     "Header right" : "Testera derecha",
-    "Content" : "Conteníu"
+    "As guest" : "Como invitáu",
+    "Footer left" : "Pie a manzorga",
+    "Footer right" : "Pie a mandrecha",
+    "At login" : "Al entamu de sesión",
+    "Application" : "Aplicación",
+    "Content" : "Conteníu",
+    "You can use plain text, markdown notation or html markup with inline style attributes. " : "Puedes utilizar testu ensin formatu, notación markdown o códigu html con atributos d'estilu."
 },
 "nplurals=2; plural=(n != 1);");
