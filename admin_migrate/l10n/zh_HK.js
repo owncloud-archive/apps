4ffac29cc94c4e14abc2b521657e60dbe1d272fc
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-07-26 00:50:58 +0200

    [tx-robot] updated from transifex

--- admin_migrate/l10n/zh_HK.js
+++ admin_migrate/l10n/zh_HK.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "admin_migrate",
     {
     "Export data" : "匯出資料",
+    "Only user data" : "只有用戶數據",
     "Export" : "輸出"
 },
 "nplurals=1; plural=0;");
--- user_migrate/l10n/zh_HK.js
+++ user_migrate/l10n/zh_HK.js
@@ -2,6 +2,6 @@ OC.L10N.register(
     "user_migrate",
     {
     "Export" : "輸出",
-    "Import" : "導入"
+    "Import" : "滙入"
 },
 "nplurals=1; plural=0;");
