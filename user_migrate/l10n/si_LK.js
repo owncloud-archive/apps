4822a6d2d3dcfdda314a86e3212359b44d543864
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-10-08 00:51:07 +0200

    [tx-robot] updated from transifex

--- user_migrate/l10n/si_LK.js
+++ user_migrate/l10n/si_LK.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "Export" : "නිර්යාත කරන්න",
     "Something went wrong while the export file was being generated" : "නිර්යාත කිරීම සඳහා ගොනුව සුදානම් කරන විට දෝෂයක් ඇති විය",
     "An error has occurred" : "දෝෂයක් ඇති විය",
+    "Account Export/Import" : "ගිණුම ආනයනය/අපනයන",
     "Import" : "ආයාත කරන්න"
 },
 "nplurals=2; plural=(n != 1);");
