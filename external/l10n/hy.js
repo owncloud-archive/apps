11dc4ecd6fd5849a1160e65d4d339bc504b0bb11
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-11-19 00:51:05 +0100

    [tx-robot] updated from transifex

--- external/l10n/hy.js
+++ external/l10n/hy.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "External Sites" : "Արտաքին կայքեր",
     "Name" : "Անուն",
     "URL" : "URL",
+    "Remove site" : "Ջնջել կայքը",
     "Add" : "Ավելացնել"
 },
 "nplurals=2; plural=(n != 1);");
