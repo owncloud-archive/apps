c7dde1fbc430f95a6c9452d027ab0177e767bba5
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-10 00:50:39 +0100

    [tx-robot] updated from transifex

--- imprint/l10n/hr.js
+++ imprint/l10n/hr.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "imprint",
     {
     "Legal notice" : "Pravne obavijesti",
-    "Nothing here yet" : "Ovdje nema ničega"
+    "Nothing here yet" : "Ovdje nema ničega",
+    "Imprint" : "Otisak"
 },
 "nplurals=3; plural=n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2;");
