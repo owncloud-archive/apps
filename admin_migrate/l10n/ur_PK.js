1831f66d5ae8901bf3ec8099f66d5609d968b280
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-03-30 00:50:39 +0200

    [tx-robot] updated from transifex

---
+++ admin_migrate/l10n/ur_PK.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "admin_migrate",
+    {
+    "Export data" : "برآمد ڈیٹا",
+    "All system files and user data" : "تمام سیسٹم فائلیں اور مستخدم ڈیٹا",
+    "Only system files" : "صرف سیسٹم فائلیں",
+    "Only user data" : "صرف مستخدم ڈیٹا",
+    "Export" : "برآمد"
+},
+"nplurals=2; plural=(n != 1);");
