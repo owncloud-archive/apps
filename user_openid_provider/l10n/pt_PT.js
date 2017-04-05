0a1294173a082eba1280be3c5830d5664b4198e8
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-04-06 00:50:37 +0200

    [tx-robot] updated from transifex

--- user_openid_provider/l10n/pt_PT.js
+++ user_openid_provider/l10n/pt_PT.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "user_openid_provider",
     {
-    "OpenID Provider remembered sites" : "Páginas de Fornecedores de OpenID lembradas",
+    "OpenID Provider remembered sites" : "Sites de Fornecedores de OpenID lembradas",
     "Trusted" : "De confiança",
     "Denied" : "Negado",
     "Remove" : "Remover"
