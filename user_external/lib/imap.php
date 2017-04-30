9603d5175729b1a805806983a904f21c68535a2c
Michael Klemme <mklemme@mklemme.de>
2017-04-30 22:54:59 +0200

    Integrated conflicting patch submitted in "Login without IMAP domain part #2166"

--- user_external/lib/imap.php
+++ user_external/lib/imap.php
@@ -16,70 +16,112 @@
  * @link     http://github.com/owncloud/apps
  */
 class OC_User_IMAP extends \OCA\user_external\Base {
-    private $mailbox;
+	private $mailbox;
+	private $domain;
 
-    /**
-     * Create new IMAP authentication provider
-     *
-     * @param string $mailbox PHP imap_open mailbox definition, e.g.
-     *                        {127.0.0.1:143/imap/readonly}
-     */
-    public function __construct($mailbox) {
-        parent::__construct($mailbox);
-        $this->mailbox=$mailbox;
-    }
+	/**
+	 * Create new IMAP authentication provider
+	 *
+	 * @param string $mailbox PHP imap_open mailbox definition, e.g.
+	 *                        {127.0.0.1:143/imap/readonly}
+	 * @param string $domain (optional) the domain users are checked against
+	 *                        users can then log in only with their local name part (before "@")
+	 */
+	public function __construct($mailbox, $domain = '') {
+		parent::__construct($mailbox);
+		$this->mailbox=$mailbox;
+		$this->domain=$domain;
+	}
 
-    /**
-     * Check if the password is correct without logging in the user
-     *
-     * @param string $uid      The username
-     * @param string $password The password
-     *
-     * @return true/false
-     */
-    public function checkPassword($uid, $password) {
-        if (!function_exists('imap_open')) {
-            OCP\Util::writeLog('user_external', 'ERROR: PHP imap extension is not installed', OCP\Util::ERROR);
-            return false;
-        }
-        $mbox = @imap_open($this->mailbox, $uid, $password, OP_HALFOPEN, 1);
-        imap_errors();
-        imap_alerts();
-        if($mbox !== FALSE) {
-            imap_close($mbox);
-            $uid = mb_strtolower($uid);
-            $this->storeUser($uid);
+	/**
+	 * Check if the password is correct without logging in the user
+	 *
+	 * @param string $uid      The username
+	 * @param string $password The password
+	 *
+	 * @return true/false
+	 */
+	public function checkPassword($uid, $password) {
+		if (!function_exists('imap_open')) {
+			OCP\Util::writeLog('user_external', 'ERROR: PHP imap extension is not installed', OCP\Util::ERROR);
+			return false;
+		}
+
+		// Check if we only want logins from ONE domain and strip the domain part from UID		
+		if($this->domain != '') {
+			$pieces = explode('@', $uid);
+			if(count($pieces) == 1) {
+                // user did not enter domain part
+				$username = $uid . "@" . $this->domain;
+                $displayname=$uid;
+                $domain=$this->domain;
+			}elseif((count($pieces) == 2) and ($pieces[1] == $this->domain)) {
+				// user did enter domain part
+				$username = $uid;
+				$uid = $pieces[0];
+                $displayname=$pieces[0];
+                $domain=$pieces[1];
+			}else{
+				return false;
+			}
             
-            if (\OC::$server->getMailer()->validateMailAddress($uid) && 
-            (preg_match('/^([^@]+)@(.+)\.[^.]+$/', $uid, $matches))) {
-              // uid is formatted username@domain.extension
-              $username=$matches[1];
-              $username=str_replace(".", " ", $username);
-              $domain=$matches[2];
-              
-              // set email adress, if not already defined
-              $userManager=\OC::$server->getUserManager();
-              $user=$userManager->get($uid);
-              $currentEmail = $user->getEMailAddress();
-              if ($currentEmail == "") {
-                $user->setEMailAddress($uid);
-              }
+		}else{
+            // check for multiple domains
+			$username = $uid;
 
-              // set Display name, if not already defined
-              $displayname=$this->getDisplayName($uid) ;
-              if ($displayname == "") {
-                $this->setDisplayName($uid, $username) ;
-              }
-
-              // create group from domain name, add user to group
-              $groupManager = \OC::$server->getGroupManager();
-              $group = $groupManager->createGroup($domain);
-              $group->addUser($uid);
+            $pieces = explode('@', $uid);
+            if(count($pieces) == 1) {
+            	$displayname= $uid;
+            	$domain="";            	
+            } else {
+            	$displayname= $pieces[0];
+            	$domain=$pieces[1];
             }
+		}
 
-            return $uid;
-        }else{
-            return false;
+		OCP\Util::writeLog('user_external', "username: $username, displayname $displayname domain: $domain"
+				, OCP\Util::INFO);
+		
+		$mbox = @imap_open($this->mailbox, $username, $password, OP_HALFOPEN, 1);
+		imap_errors();
+		imap_alerts();
+		if($mbox === FALSE) {
+			imap_close($mbox);
+			return false;			
+		}
+		
+		imap_close($mbox);
+		$uid = mb_strtolower($uid);
+		$this->storeUser($uid);
+
+        //// store extra information based on the email address
+
+        // set email adress, if not already defined
+        $userManager=\OC::$server->getUserManager();
+        $user=$userManager->get($uid);
+        $currentEmail = $user->getEMailAddress();
+        if ($currentEmail == "") {
+            $user->setEMailAddress($username);
         }
-    }
+             
+        // set Display name, if not already defined
+        $currentDisplayname=$this->getDisplayName($uid) ;
+        if ($currentDisplayname == "" || $currentDisplayname == $username) {
+        	$displayname=strtr($displayname, ".", " ");
+            $this->setDisplayName($uid, $displayname) ;
+        }
+            
+        if($this->domain == '' && $domain != '') {
+            // create group from domain name, add user to group
+            $groupManager = \OC::$server->getGroupManager();
+            // drop TLD from domain
+            $domain= preg_replace('/\..*?$/', '', $domain);
+            OCP\Util::writeLog('user_external', "domain: $domain"
+            		, OCP\Util::ERROR);
+            $group = $groupManager->createGroup($domain);
+            $group->addUser($user);
+        }
+
+        return $uid;		
+	}
 }
