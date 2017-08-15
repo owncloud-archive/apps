d8c6aed5d286e3cb60dac996f8096713d6332937
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-08-16 00:50:50 +0200

    [tx-robot] updated from transifex

--- user_migrate/l10n/af_ZA.js
+++ user_migrate/l10n/af_ZA.js
@@ -1,6 +1,10 @@
 OC.L10N.register(
     "user_migrate",
     {
-    "Export" : "Uitvoer"
+    "Export" : "Uitstuur",
+    "Something went wrong while the export file was being generated" : "Iets het skeefgeloop toe die uitstuurlêer geskep is",
+    "An error has occurred" : "’n Fout het voorgekom",
+    "Account Export/Import" : "Rekeninguitstuur/-invoer",
+    "Import" : "Invoer"
 },
 "nplurals=2; plural=(n != 1);");
---
+++ user_openid_provider/l10n/af_ZA.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "user_openid_provider",
+    {
+    "OpenID Provider remembered sites" : "OpenID-verskaffer onthoude werwe",
+    "Trusted" : "Vertroud",
+    "Denied" : "Geweier",
+    "Remove" : "Verwyder"
+},
+"nplurals=2; plural=(n != 1);");
