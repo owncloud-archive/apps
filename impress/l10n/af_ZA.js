a3e44e5afc54b61ba66f794e280047616031abbb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2017-08-25 00:50:51 +0200

    [tx-robot] updated from transifex

--- impress/l10n/af_ZA.js
+++ impress/l10n/af_ZA.js
@@ -2,6 +2,7 @@ OC.L10N.register(
     "impress",
     {
     "Documentation" : "Dokumentasie",
+    "No Impress files are found in your ownCloud. Please upload a .impress file." : "Geen Indruk-lêers is in u ownCloud gevind nie. Laai asb. ’n .impress-lêer op.",
     "Name" : "Naam",
     "Size" : "Grootte",
     "Date" : "Datum"
--- ownpad_lite/l10n/af_ZA.js
+++ ownpad_lite/l10n/af_ZA.js
@@ -1,8 +1,15 @@
 OC.L10N.register(
     "ownpad_lite",
     {
+    "Document was shared" : "Dokument is gedeel",
+    "User %s shared quick document %s with you." : "Gebruiker %s het kitsdokument %s met u gedeel.",
+    "My pad" : "My pad",
     "Settings" : "Instellings",
+    "Pad Title" : "Padtitel",
+    "Open" : "Open",
+    "Share with" : "Deel met",
     "Share" : "Deel",
+    "Etherpad service URL" : "Etherpaddiensbronadres",
     "Username" : "Gebruikersnaam",
     "Save" : "Bewaar"
 },
