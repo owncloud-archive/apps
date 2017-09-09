5e461d4db13e0eb4f7ae89356e1f0d0b724d05ec
Nick Sweeting <git@nicksweeting.com>
2017-09-09 17:53:21 -0500

    save config arrays to variables

--- external/settings.php
+++ external/settings.php
@@ -26,8 +26,14 @@ if (file_exists(\OC::$SERVERROOT . "/themes/$theme/apps/external/img/")) {
 }
 
 $tmpl->assign('images', $images);
-$tmpl->assign('targets', array('_blank', '_top', '_self'));
-$tmpl->assign('targets_desc', array('New Window', 'Replace Current Window', 'Inside OwnCloud Frame'));
+
+// _blank opens link in a new window/tab
+// _top replaces the current owncloud window with link
+// _self opens link in the ownCloud iframe
+$targets = array('_blank', '_top', '_self');
+$targets_desc = array('New Window', 'Replace Current Window', 'Inside OwnCloud Frame');
+$tmpl->assign('targets', $targets);
+$tmpl->assign('targets_desc', $targets_desc);
 
 
 return $tmpl->fetchPage();
