5726f3ecb40f418a2407575019d5d95962acfb32
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-03-14 00:50:47 +0100

    [tx-robot] updated from transifex

---
+++ admin_migrate/l10n/lo.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "admin_migrate",
+    {
+    "Export data" : "ນຳອອກຂໍ້ມູນ",
+    "All system files and user data" : "ໄຟຣ໌ລະບົບທັງໝົດ ແລະ ຂໍ້ມູນຜູ້ໃຊ້",
+    "Only system files" : "ໄຟຣ໌ລະບົບເທົ່ານັ້ນ",
+    "Only user data" : "ຂໍ້ມູນຜູ້ໃຊ້ເທົ້ານັ້ນ",
+    "Export" : "ນຳອອກ"
+},
+"nplurals=1; plural=0;");
