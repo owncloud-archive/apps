4305ad3acd78f3e2be18b7c59c4bb44863cb7b3b
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-03-22 00:50:51 +0100

    [tx-robot] updated from transifex

--- external/l10n/mk.js
+++ external/l10n/mk.js
@@ -1,6 +1,7 @@
 OC.L10N.register(
     "external",
     {
+    "Please enter valid urls - they have to start with either http://, https:// or /" : "Внесете валидни адреси - тие треба да започнат со http://, https:// или /",
     "External Sites" : "Надворешни сајтови",
     "Name" : "Име",
     "URL" : "Адреса",
