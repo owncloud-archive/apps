bf3846ea1e9790ac65aabeb0f6493f15de897caf
Vincent Petry <pvince81@yahoo.fr>
2018-11-13 12:30:54 +0100

    Remove user_external app

--- user_external/appinfo/app.php
+++
@@ -1,4 +0,0 @@
-<?php
-OC::$CLASSPATH['OC_User_IMAP']='user_external/lib/imap.php';
-OC::$CLASSPATH['OC_User_SMB']='user_external/lib/smb.php';
-OC::$CLASSPATH['OC_User_FTP']='user_external/lib/ftp.php';
